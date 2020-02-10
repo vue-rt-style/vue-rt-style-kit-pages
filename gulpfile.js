a7d214aaa5394932baca399b0a53e9595c97654e
cryingjoker <cryingjoker@me.com>
2020-02-10 10:57:29 +0300

    deploy docs

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task(
         'perl -pi -w -e \'s/\\<\\/script\\>\\<\\/head\\>/\\<\\/script\\>\\<link rel\\=\\"stylesheet\\" href\\=\\"\\/vue-rt-style-kit-pages\\/fonts.css\\"\\/\\>\\<\\/head\\>/g\' ./docs/index.html\n',
         "perl -p -i -e 's/docs\\/\"vendors/docs\\/vendors/g' ./docs/index.html\n",
         "perl -p -i -e 's/docs\\/\"main/docs\\/main/g' ./docs/index.html\n",
-        "perl -p -i -e 's/src=\\/vue-rt-style-kit-pages/src=\"\\/vue-rt-style-kit-pages/g' ./docs/index.html\n",
+        // "perl -p -i -e 's/src=\\/vue-rt-style-kit-pages/src=\"\\/vue-rt-style-kit-pages/g' ./docs/index.html\n",
         "rm -rf ./dist\n",
         "git commit -a -m'deploy docs'\n",
         "git push\n",
