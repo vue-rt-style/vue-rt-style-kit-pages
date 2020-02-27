d010d1e59f49b302fc027a835f5fb7f28c7c7499
cryingjoker <cryingjoker@me.com>
2020-02-27 12:26:24 +0300

    add codeeditor in textarea example

--- src/example-pages/components/main-elements/Textarea.vue
+++ src/example-pages/components/main-elements/Textarea.vue
@@ -19,21 +19,23 @@
             <span class="rt-font-label flex-fill rt-space-bottom05"
               >simple static textarea</span
             >
-            <rt-textarea-static
+            <pre-code
+                    text='<rt-textarea-static
               placeholder="Textarea"
               label="Введите комментарий:"
-            />
+            />'></pre-code>
           </div>
           <div class="rt-col-6  rt-col-md-3 rt-md-space-top">
             <span class="rt-font-label flex-fill rt-space-bottom05"
               >static textarea with error</span
             >
-            <rt-textarea-static
+            <pre-code
+                    text='<rt-textarea-static
               :has-error="true"
               label="Введите комментарий:"
               error-message="textarea error message"
               placeholder="Textarea"
-            />
+            ></rt-textarea-static'></pre-code>
           </div>
         </div>
 
@@ -42,52 +44,58 @@
             <span class="rt-font-label flex-fill rt-space-bottom05"
               >static disabled textarea</span
             >
-            <rt-textarea-static
+            <pre-code
+                    text='<rt-textarea-static
               :disabled="true"
               label="Введите комментарий:"
               error-message="textarea error message"
               placeholder="Textarea"
-            />
+            ></rt-textarea-static>'></pre-code>
           </div>
         </div>
         <div class="app-row row">
           <div class="rt-col-4  rt-col-md-2 rt-md-space-top d-flex flex-column">
             <span class="rt-font-label flex-fill rt-space-bottom05">dynamic textarea</span>
-            <rt-textarea placeholder="Textarea" />
+            <pre-code
+                    text='<rt-textarea placeholder="Textarea" ></rt-textarea>'></pre-code>
           </div>
           <div class="rt-col-4  rt-col-md-2 rt-md-space-top d-flex flex-column">
             <span class="rt-font-label flex-fill rt-space-bottom05">dynamic textarea with error</span>
-            <rt-textarea
+            <pre-code
+                    text='<rt-textarea
               :has-error="true"
               label="Введите комментарий:"
               placeholder="Textarea"
               error-message="textarea error message"
-            />
+            ></rt-textarea>'></pre-code>
           </div>
 
           <div class="rt-col-4  rt-col-md-1 rt-md-space-top d-flex flex-column">
             <span class="rt-font-label flex-fill rt-space-bottom05">dynamic disabled textarea</span>
-            <rt-textarea
+            <pre-code
+                    text='<rt-textarea
               :disabled="true"
               label="Введите комментарий:"
               placeholder="Textarea"
               error-message="textarea error message"
-            />
+            ></rt-textarea>'></pre-code>
           </div>
         </div>
         <div
           class="app-row row color-block--light-grey rt-space-bottom rt-space-top"
         >
           <div class="rt-col-4  rt-col-md-2 rt-md-space-top">
-            <rt-textarea placeholder="Textarea" :is-b2b-textarea="true" />
+            <pre-code
+                    text='<rt-textarea placeholder="Textarea" :is-b2b-textarea="true" ></rt-textarea>'></pre-code>
           </div>
           <div class="rt-col-4  rt-col-md-2 rt-md-space-top d-flex flex-column">
-            <rt-textarea
+            <pre-code
+                    text='<rt-textarea
               :has-error="true"
               :is-b2b-textarea="true"
               placeholder="Textarea"
               error-message="textarea error message"
-            />
+            ></rt-textarea>'></pre-code>
           </div>
 
           <div class="rt-col-4  rt-col-md-1 rt-md-space-top d-flex flex-column">
@@ -96,35 +104,38 @@
               :is-b2b-textarea="true"
               placeholder="Textarea"
               error-message="textarea error message"
-            />
+            ></rt-textarea>
           </div>
         </div>
         <div class="app-row row">
           <div class="rt-col-4  rt-col-md-2 rt-md-space-top">
-            <rt-textarea
+            <pre-code
+                    text='<rt-textarea
               placeholder="Textarea"
               :is-b2b-textarea="true"
               :outlined="true"
-            />
+            ></rt-textarea>'></pre-code>
           </div>
           <div class="rt-col-4  rt-col-md-2 rt-md-space-top d-flex flex-column">
-            <rt-textarea
+            <pre-code
+                    text='<rt-textarea
               :has-error="true"
               :is-b2b-textarea="true"
               placeholder="Textarea"
               error-message="textarea error message"
               :outlined="true"
-            />
+            ></rt-textarea>'></pre-code>
           </div>
 
           <div class="rt-col-4  rt-col-md-1 rt-md-space-top d-flex flex-column">
-            <rt-textarea
+            <pre-code
+                    text='<rt-textarea
               :disabled="true"
               :is-b2b-textarea="true"
               placeholder="Textarea"
               error-message="textarea error message"
               :outlined="true"
-            />
+            ></rt-textarea>'></pre-code>
           </div>
         </div>
       </div>
