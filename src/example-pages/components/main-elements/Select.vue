d4576582888c190da8877891111b3c5bc0d88f04
cryingjoker <cryingjoker@me.com>
2020-03-03 15:08:54 +0300

    add select props for content text + add selected props in select-option component

--- src/example-pages/components/main-elements/Select.vue
+++ src/example-pages/components/main-elements/Select.vue
@@ -15,13 +15,24 @@
           </div>
         </div>
         <div class="app-row row">
+          <div class="rt-col-3 rt-col-md-3">
+            <span class="rt-font-label flex-fill rt-space-bottom05">simple with content text</span>
+            <rt-select label="Оборудование" value="0" :has-content-text="true">
+              <template slot="content"><p class="rt-font-control color-main05 rt-space-top0-half">Пакет входящих минут <br> с любых телефонов России</p></template>
+              <rt-select-option :selected="true" :value="150">150 минут</rt-select-option>
+              <rt-select-option  :value="150">500 минут</rt-select-option>
+              <rt-select-option  :value="150">1000 минут</rt-select-option>
+            </rt-select>
+          </div>
+        </div>
+        <div class="app-row row">
           <div class="col rt-col-md-3">
             <span class="rt-font-label flex-fill rt-space-bottom05">simple select</span>
             <rt-select label="Оборудование" value="0">
               <rt-select-option v-for="(option, index) in optionsList"
                                 :key="'index' + Math.random().toString(5).slice(4)"
                                 :value="String(index)">
-                {{ option }}
+                {{ option.label }}
               </rt-select-option>
             </rt-select>
           </div>
@@ -37,7 +48,7 @@
               <rt-select-option v-for="(option, index) in optionsList"
                                 :key="'index' + Math.random().toString(5).slice(4)"
                                 :value="String(index)">
-                {{ option }}
+                {{ option.label }}
               </rt-select-option>
             </rt-select>
           </div>
@@ -50,7 +61,7 @@
               <rt-select-option v-for="(option, index) in optionsList"
                                 :key="'index' + Math.random().toString(5).slice(4)"
                                 :value="String(index)">
-                {{ option }}
+                {{ option.label }}
               </rt-select-option>
             </rt-select>
           </div>
@@ -61,7 +72,7 @@
               <rt-select-option v-for="(option, index) in optionsList"
                                 :key="'index' + Math.random().toString(5).slice(4)"
                                 :value="String(index)">
-                {{ option }}
+                {{ option.label }}
               </rt-select-option>
             </rt-select>
           </div>
@@ -72,7 +83,7 @@
               <rt-select-option v-for="(option, index) in optionsList"
                                 :key="'index' + Math.random().toString(5).slice(4)"
                                 :value="String(index)">
-                {{ option }}
+                {{ option.label }}
               </rt-select-option>
             </rt-select>
           </div>
@@ -85,7 +96,7 @@
               <rt-select-option v-for="(option, index) in optionsList"
                                 :key="'index' + Math.random().toString(5).slice(4)"
                                 :value="String(index)">
-                {{ option }}
+                {{ option.label }}
               </rt-select-option>
             </rt-select>
           </div>
@@ -97,7 +108,7 @@
               <rt-select-option v-for="(option, index) in optionsList"
                                 :key="'index' + Math.random().toString(5).slice(4)"
                                 :value="String(index)">
-                {{ option }}
+                {{ option.label }}
               </rt-select-option>
             </rt-select>
           </div>
@@ -111,7 +122,7 @@
               <rt-select-option v-for="(option, index) in optionsList"
                                 :key="'index' + Math.random().toString(5).slice(4)"
                                 :value="String(index)">
-                {{ option }}
+                {{ option.label }}
               </rt-select-option>
             </rt-select>
           </div>
@@ -123,7 +134,7 @@
               <rt-select-option v-for="(option, index) in optionsList"
                                 :key="'index' + Math.random().toString(5).slice(4)"
                                 :value="String(index)">
-                {{ option }}
+                {{ option.label }}
               </rt-select-option>
             </rt-select>
           </div>
@@ -152,89 +163,89 @@ export default {
   data: () => ({
     inputModelValue: "test",
     optionsList: [
-      "Адыгея Респ",
-      "Алтай Респ",
-      "Алтайский край",
-      "Амурская обл",
-      "Архангельская обл",
-      "Астраханская обл",
-      "Башкортостан Респ",
-      "Белгородская обл",
-      "Брянская обл",
-      "Бурятия Респ",
-      "Владимирская обл",
-      "Волгоградская обл",
-      "Вологодская обл",
-      "Воронежская обл",
-      "Дагестан Респ",
-      "Еврейская Аобл",
-      "Забайкальский край",
-      "Ивановская обл",
-      "Ингушетия Респ",
-      "Иркутская обл",
-      "Кабардино-Балкарская Респ",
-      "Калининградская обл",
-      "Калмыкия Респ",
-      "Калужская обл",
-      "Камчатский край",
-      "Карачаево-Черкесская Респ",
-      "Карелия Респ",
-      "Кемеровская обл",
-      "Кировская обл",
-      "Коми Респ",
-      "Костромская обл",
-      "Краснодарский край",
-      "Красноярский край",
-      "Курганская обл",
-      "Курская обл",
-      "Ленинградская обл",
-      "Липецкая обл",
-      "Магаданская обл",
-      "Марий Эл Респ",
-      "Мордовия Респ",
-      "Москва г",
-      "Московская обл",
-      "Мурманская обл",
-      "Ненецкий АО",
-      "Нижегородская обл",
-      "Новгородская обл",
-      "Новосибирская обл",
-      "Омская обл",
-      "Оренбургская обл",
-      "Орловская обл",
-      "Пензенская обл",
-      "Пермский край",
-      "Приморский край",
-      "Псковская обл",
-      "Ростовская обл",
-      "Рязанская обл",
-      "Самарская обл",
-      "Санкт-Петербург г",
-      "Саратовская обл",
-      "Саха /Якутия/ Респ",
-      "Сахалинская обл",
-      "Свердловская обл",
-      "Северная Осетия - Алания Респ",
-      "Смоленская обл",
-      "Ставропольский край",
-      "Тамбовская обл",
-      "Татарстан Респ",
-      "Тверская обл",
-      "Томская обл",
-      "Тульская обл",
-      "Тыва Респ",
-      "Тюменская обл",
-      "Удмуртская Респ",
-      "Ульяновская обл",
-      "Хабаровский край",
-      "Хакасия Респ",
-      "Ханты-Мансийский Автономный округ - Югра АО",
-      "Челябинская обл",
-      "Чеченская Респ",
-      "Чувашская Респ",
-      "Чукотский АО",
-      "Ямало-Ненецкий АО",
-      "Ярославская обл"
+      {label:"Адыгея Респ",selected:true},
+      {label:"Алтай Респ",selected:false},
+      {label:"Алтайский край",selected:false},
+      {label:"Амурская обл",selected:false},
+      {label:"Архангельская обл",selected:false},
+      {label:"Астраханская обл",selected:false},
+      {label:"Башкортостан Респ",selected:false},
+      {label:"Белгородская обл",selected:false},
+      {label:"Брянская обл",selected:false},
+      {label:"Бурятия Респ",selected:false},
+      {label:"Владимирская обл",selected:false},
+      {label:"Волгоградская обл",selected:false},
+      {label:"Вологодская обл",selected:false},
+      {label:"Воронежская обл",selected:false},
+      {label:"Дагестан Респ",selected:false},
+      {label:"Еврейская Аобл",selected:false},
+      {label:"Забайкальский край",selected:false},
+      {label:"Ивановская обл",selected:false},
+      {label:"Ингушетия Респ",selected:false},
+      {label:"Иркутская обл",selected:false},
+      {label:"Кабардино-Балкарская Респ",selected:false},
+      {label:"Калининградская обл",selected:false},
+      {label:"Калмыкия Респ",selected:false},
+      {label:"Калужская обл",selected:false},
+      {label:"Камчатский край",selected:false},
+      {label:"Карачаево-Черкесская Респ",selected:false},
+      {label:"Карелия Респ",selected:false},
+      {label:"Кемеровская обл",selected:false},
+      {label:"Кировская обл",selected:false},
+      {label:"Коми Респ",selected:false},
+      {label:"Костромская обл",selected:false},
+      {label:"Краснодарский край",selected:false},
+      {label:"Красноярский край",selected:false},
+      {label:"Курганская обл",selected:false},
+      {label:"Курская обл",selected:false},
+      {label:"Ленинградская обл",selected:false},
+      {label:"Липецкая обл",selected:false},
+      {label:"Магаданская обл",selected:false},
+      {label:"Марий Эл Респ",selected:false},
+      {label:"Мордовия Респ",selected:false},
+      {label:"Москва г",selected:false},
+      {label:"Московская обл",selected:false},
+      {label:"Мурманская обл",selected:false},
+      {label:"Ненецкий АО",selected:false},
+      {label:"Нижегородская обл",selected:false},
+      {label:"Новгородская обл",selected:false},
+      {label:"Новосибирская обл",selected:false},
+      {label:"Омская обл",selected:false},
+      {label:"Оренбургская обл",selected:false},
+      {label:"Орловская обл",selected:false},
+      {label:"Пензенская обл",selected:false},
+      {label:"Пермский край",selected:false},
+      {label:"Приморский край",selected:false},
+      {label:"Псковская обл",selected:false},
+      {label:"Ростовская обл",selected:false},
+      {label:"Рязанская обл",selected:false},
+      {label:"Самарская обл",selected:false},
+      {label:"Санкт-Петербург г",selected:false},
+      {label:"Саратовская обл",selected:false},
+      {label:"Саха /Якутия/ Респ",selected:false},
+      {label:"Сахалинская обл",selected:false},
+      {label:"Свердловская обл",selected:false},
+      {label:"Северная Осетия - Алания Респ",selected:false},
+      {label:"Смоленская обл",selected:false},
+      {label:"Ставропольский край",selected:false},
+      {label:"Тамбовская обл",selected:false},
+      {label:"Татарстан Респ",selected:false},
+      {label:"Тверская обл",selected:false},
+      {label:"Томская обл",selected:false},
+      {label:"Тульская обл",selected:false},
+      {label:"Тыва Респ",selected:false},
+      {label:"Тюменская обл",selected:false},
+      {label:"Удмуртская Респ",selected:false},
+      {label:"Ульяновская обл",selected:false},
+      {label:"Хабаровский край",selected:false},
+      {label:"Хакасия Респ",selected:false},
+      {label:"Ханты-Мансийский Автономный округ - Югра АО",selected:false},
+      {label:"Челябинская обл",selected:false},
+      {label:"Чеченская Респ",selected:false},
+      {label:"Чувашская Респ",selected:false},
+      {label:"Чукотский АО",selected:false},
+      {label:"Ямало-Ненецкий АО",selected:false},
+      {label:"Ярославская обл",selected:false},
     ]
   }),
   mounted() {
