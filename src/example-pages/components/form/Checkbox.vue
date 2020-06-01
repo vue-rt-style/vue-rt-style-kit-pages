b1e9c656b231e0824aecc0461f4d7ac60fd9ba10
cryingjoker <cryingjoker@me.com>
2020-06-01 17:26:10 +0300

    add orange props in checkebox + part of checkbox style done

--- src/example-pages/components/form/Checkbox.vue
+++ src/example-pages/components/form/Checkbox.vue
@@ -1,44 +1,71 @@
 <template>
-  <div class="rt-container">
-    <div class="app-content rt-col-12 rt-col-md-3">
-      <div class="app-title">
-        <h1 class="rt-font-hero">
-          Checkbox
-        </h1>
-      </div>
-      <div class="app-body">
-        <div class="app-row row">
-          <p class="rt-font-h3 rt-col">
-            checkbox
-          </p>
-        </div>
-        <div class="app-row row">
-          <pre-code
-            text="<div class=&quot;rt-col&quot;><rt-checkbox :validate=&quot;{required: true}&quot; :required=&quot;true&quot; name=&quot;internet&quot;>Интернет</rt-checkbox><div class=&quot;rt-space-vertical&quot;><rt-checkbox name=&quot;sms&quot;>Смс</rt-checkbox></div><rt-checkbox name=&quot;voiseMessage&quot;>Голосовая почта</rt-checkbox><div class=&quot;rt-space-vertical&quot;><rt-checkbox name=&quot;calls&quot; :checked=&quot;true&quot;>Звонки</rt-checkbox></div></div>"
-          />
-        </div>
-        <div class="app-row row">
-          <p class="rt-font-h3 rt-col">
-            disabled checkbox
-          </p>
-        </div>
-        <div class="app-row row">
-          <pre-code
-            text="<div class=&quot;rt-col&quot;><div class=&quot;rt-space-vertical&quot;><rt-checkbox :is-disabled=&quot;true&quot;>Интернет</rt-checkbox></div><rt-checkbox :is-disabled=&quot;true&quot; :checked=&quot;true&quot;>Интернет</rt-checkbox></div>"
-          />
-        </div>
-      </div>
-      <div class="app-row row rt-space-top">
-        <documentation-builder :json="documentation" type="components"/>
-      </div>
-    </div>
-  </div>
+	<div class="rt-container">
+		<div class="app-content rt-col-12 rt-col-md-3">
+			<div class="app-title">
+				<h1 class="rt-font-hero">
+					Checkbox
+				</h1>
+			</div>
+			<div class="app-body">
+				<div class="app-row row">
+					<p class="rt-font-h3 rt-col">
+						checkbox
+					</p>
+				</div>
+				<div class="app-row row">
+					<rt-col :size="4">
+						<pre-code
+								text='<rt-checkbox :validate="{required: true}" :required="true" name="internet">Интернет</rt-checkbox>'/>
+						<div class="rt-space-vertical">
+							<pre-code text='<rt-checkbox name="sms">Смс</rt-checkbox>'></pre-code>
+						</div>
+						<pre-code text='<rt-checkbox name="voiseMessage">Голосовая почта</rt-checkbox>'></pre-code>
+						<div class="rt-space-vertical">
+							<pre-code text='<rt-checkbox name="calls" :checked="true">Звонки</rt-checkbox>'></pre-code>
+						</div>
+					</rt-col>
+					<rt-col :size="2"></rt-col>
+					<rt-col :size="4">
+						<pre-code
+								text='<rt-checkbox :is-orange="true" :validate="{required: true}" :required="true" name="internet">Интернет</rt-checkbox>'></pre-code>
+						<div class="rt-space-vertical">
+							<pre-code text='<rt-checkbox :is-orange="true" name="sms">Смс</rt-checkbox>'></pre-code>
+						</div>
+						<rt-checkbox name="voiseMessage">Голосовая почта</rt-checkbox>
+						<div class="rt-space-vertical">
+							<pre-code
+									text='<rt-checkbox :is-orange="true" name="calls" :checked="true">Звонки</rt-checkbox>'></pre-code>
+						</div>
+					</rt-col>
+
+				</div>
+				<div class="app-row row">
+					<p class="rt-font-h3 rt-col">
+						disabled checkbox
+					</p>
+				</div>
+				<div class="app-row row">
+					<pre-code
+							text='<rt-col>
+          <div class="rt-space-vertical">
+            <rt-checkbox :is-disabled="true">Интернет</rt-checkbox>
+          </div>
+          <rt-checkbox :is-disabled="true" :checked="true">Интернет</rt-checkbox>
+        </rt-col>'
+					/>
+				</div>
+			</div>
+			<div class="app-row row rt-space-top">
+				<documentation-builder :json="documentation" type="components"/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
   import documentation from "@vue-rt-style-kit-atoms-local/components/Checkbox/docs/index.json";
   import componentsList from "../../componentsList";
-//  const componentsList = {};
+  //  const componentsList = {};
   export default {
     name: "AppCheckbox",
     components: componentsList,
@@ -48,7 +75,8 @@
     mounted() {
       this.documentation = documentation;
     },
-    created() {},
+    created() {
+    },
     methods: {}
   };
 </script>
