ed49d5d48791f08dae98c2d4f8a3b96b5bb3009b
cryingjoker <cryingjoker@me.com>
2020-06-08 15:27:10 +0300

    done checkbox new style

--- src/example-pages/components/form/Checkbox.vue
+++ src/example-pages/components/form/Checkbox.vue
@@ -1,65 +1,120 @@
 <template>
-	<div class="rt-container">
-		<div class="app-content rt-col-12 rt-col-md-3">
-			<div class="app-title">
-				<h1 class="rt-font-hero">
-					Checkbox
-				</h1>
-			</div>
-			<div class="app-body">
-				<div class="app-row row">
-					<p class="rt-font-h3 rt-col">
-						checkbox
-					</p>
+	<div>
+		<div class="rt-container">
+			<div class="app-content rt-col-12 rt-col-md-3">
+				<div class="app-title">
+					<h1 class="rt-font-hero">
+						Checkbox
+					</h1>
 				</div>
-				<div class="app-row row">
-					<rt-col :size="4">
-						<pre-code
-								text='<rt-checkbox :validate="{required: true}" :required="true" name="internet">Интернет</rt-checkbox>'/>
-						<div class="rt-space-vertical">
-							<pre-code text='<rt-checkbox name="sms">Смс</rt-checkbox>'></pre-code>
-						</div>
-						<pre-code text='<rt-checkbox name="voiseMessage">Голосовая почта</rt-checkbox>'></pre-code>
-						<div class="rt-space-vertical">
-							<pre-code text='<rt-checkbox name="calls" :checked="true">Звонки</rt-checkbox>'></pre-code>
-						</div>
-					</rt-col>
-					<rt-col :size="2"></rt-col>
-					<rt-col :size="4">
-						<pre-code
-								text='<rt-checkbox :is-orange="true" :validate="{required: true}" :required="true" name="internet">Интернет</rt-checkbox>'></pre-code>
-						<div class="rt-space-vertical">
-							<pre-code text='<rt-checkbox :is-orange="true" name="sms">Смс</rt-checkbox>'></pre-code>
-						</div>
-						<rt-checkbox name="voiseMessage">Голосовая почта</rt-checkbox>
-						<div class="rt-space-vertical">
+				<div class="app-body">
+					<rt-row class="app-row">
+						<p class="rt-font-h3 rt-col">
+							checkbox
+						</p>
+					</rt-row>
+					<rt-row class="app-row">
+						<rt-col :size="4">
 							<pre-code
-									text='<rt-checkbox :is-orange="true" name="calls" :checked="true">Звонки</rt-checkbox>'></pre-code>
-						</div>
-					</rt-col>
-
-				</div>
-				<div class="app-row row">
-					<p class="rt-font-h3 rt-col">
-						disabled checkbox
-					</p>
-				</div>
-				<div class="app-row row">
-					<pre-code
-							text='<rt-col>
-          <div class="rt-space-vertical">
-            <rt-checkbox :is-disabled="true">Интернет</rt-checkbox>
-          </div>
-          <rt-checkbox :is-disabled="true" :checked="true">Интернет</rt-checkbox>
-        </rt-col>'
-					/>
+									text='<rt-checkbox :validate="{required: true}" :required="true" name="internet">Интернет</rt-checkbox>'/>
+							<div class="rt-space-vertical">
+								<pre-code text='<rt-checkbox name="sms">Смс</rt-checkbox>'></pre-code>
+							</div>
+							<pre-code text='<rt-checkbox name="voiseMessage">Голосовая почта</rt-checkbox>'></pre-code>
+							<div class="rt-space-vertical">
+								<pre-code text='<rt-checkbox name="calls" :checked="true">Звонки</rt-checkbox>'></pre-code>
+							</div>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox :is-disabled="true">Интернет</rt-checkbox>'></pre-code>
+							</div>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox :is-disabled="true" :checked="true">Интернет</rt-checkbox>'></pre-code>
+							</div>
+						</rt-col>
+						<rt-col :size="2"></rt-col>
+						<rt-col :size="4">
+							<pre-code
+									text='<rt-checkbox :is-orange="true" :validate="{required: true}" :required="true" name="internet">Интернет</rt-checkbox>'></pre-code>
+							<div class="rt-space-vertical">
+								<pre-code text='<rt-checkbox :is-orange="true" name="sms">Смс</rt-checkbox>'></pre-code>
+							</div>
+							<rt-checkbox :is-orange="true" name="voiseMessage">Голосовая почта</rt-checkbox>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox :is-orange="true" name="calls" :checked="true">Звонки</rt-checkbox>'></pre-code>
+							</div>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox is-orange="true" :is-disabled="true">Интернет</rt-checkbox>'></pre-code>
+							</div>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox is-orange="true" :is-disabled="true" :checked="true">Интернет</rt-checkbox>'></pre-code>
+							</div>
+						</rt-col>
+					</rt-row>
 				</div>
 			</div>
+		</div>
+		<div class="color-block--dark-blue rt-space-vertical2">
+			<div class="rt-container">
+				<rt-col :size="12">
+					<rt-row class="app-row">
+						<rt-col :size="4">
+							<pre-code
+									text='<rt-checkbox :bright="true" :validate="{required: true}" :required="true" name="internet">Интернет</rt-checkbox>'/>
+							<div class="rt-space-vertical">
+								<pre-code text='<rt-checkbox :bright="true" name="sms">Смс</rt-checkbox>'></pre-code>
+							</div>
+							<pre-code text='<rt-checkbox :bright="true" name="voiseMessage">Голосовая почта</rt-checkbox>'></pre-code>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox :bright="true" name="calls" :checked="true">Звонки</rt-checkbox>'></pre-code>
+							</div>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox :bright="true" :is-disabled="true">Интернет</rt-checkbox>'></pre-code>
+							</div>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox :bright="true" :checked="true" :is-disabled="true">Интернет</rt-checkbox>'></pre-code>
+							</div>
+						</rt-col>
+						<rt-col :size="2"></rt-col>
+						<rt-col :size="4">
+							<pre-code
+									text='<rt-checkbox :bright="true" :is-orange="true" :validate="{required: true}" :required="true" name="internet">Интернет</rt-checkbox>'></pre-code>
+							<div class="rt-space-vertical">
+								<pre-code text='<rt-checkbox :bright="true" :is-orange="true" name="sms">Смс</rt-checkbox>'></pre-code>
+							</div>
+							<rt-checkbox :bright="true" :is-orange="true" name="voiseMessage">Голосовая почта</rt-checkbox>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox :bright="true" :is-orange="true" name="calls" :checked="true">Звонки</rt-checkbox>'></pre-code>
+							</div>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox :bright="true" :is-disabled="true">Интернет</rt-checkbox>'></pre-code>
+							</div>
+							<div class="rt-space-vertical">
+								<pre-code
+										text='<rt-checkbox :is-orange="true" :checked="true" :bright="true" :is-disabled="true">Интернет</rt-checkbox>'></pre-code>
+							</div>
+						</rt-col>
+					</rt-row>
+				</rt-col>
+			</div>
+		</div>
+		<div class="rt-container">
 			<div class="app-row row rt-space-top">
 				<documentation-builder :json="documentation" type="components"/>
 			</div>
 		</div>
+
 	</div>
+
 </template>
 
 <script>
