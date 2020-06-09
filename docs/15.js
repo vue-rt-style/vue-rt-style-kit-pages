459085fbc3101e0ea19ebd8388a9cc66cfb0db99
cryingjoker <cryingjoker@me.com>
2020-06-09 14:58:02 +0300

    deploy docs

--- docs/15.js
+++ docs/15.js
@@ -65,6 +65,74 @@ var _vue_rt_style_kit_atoms_local_components_RadioButton_docs_index_json__WEBPAC
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
  // const componentsList = {};
 
@@ -81,18 +149,14 @@ var _vue_rt_style_kit_atoms_local_components_RadioButton_docs_index_json__WEBPAC
     this.documentation = _vue_rt_style_kit_atoms_local_components_RadioButton_docs_index_json__WEBPACK_IMPORTED_MODULE_0__;
   },
   created: function created() {},
-  methods: {
-    setTestModel: function setTestModel(value) {
-      this.testModel = value;
-    }
-  }
+  methods: {}
 });
 
 /***/ }),
 
-/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=4171d250&":
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=1409d5f8&":
 /*!************************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=4171d250& ***!
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=1409d5f8& ***!
   \************************************************************************************************************************************************************************************************************************/
 /*! exports provided: render, staticRenderFns */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
@@ -101,8 +165,8 @@ var _vue_rt_style_kit_atoms_local_components_RadioButton_docs_index_json__WEBPAC
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"rt-container"},[_c('div',{staticClass:"app-content rt-col-12 rt-col-md-3"},[_vm._m(0),_vm._v(" "),_c('div',{staticClass:"app-body"},[_vm._m(1),_vm._v(" "),_c('div',{staticClass:"rt-space-bottom2"},[_vm._v("testModel = "+_vm._s(_vm.testModel))]),_vm._v(" "),_c('pre-code',{attrs:{"text":"<div class=\"app-row row rt-space-top2\"><div class=\"rt-col\"><rt-radio-button name=\"type\" @change=\"setTestModel\" value=\"0\" :model=\"testModel\">Огонь</rt-radio-button><div class=\"rt-space-vertical\"><rt-radio-button name=\"type\" @change=\"setTestModel\" value=\"1\" :model=\"testModel\">Вода</rt-radio-button></div><rt-radio-button name=\"type\" @change=\"setTestModel\" value=\"2\" :model=\"testModel\">Медные трубы</rt-radio-button></div></div>"}}),_vm._v(" "),_c('div',{staticClass:"app-row row"},[_c('div',{staticClass:"rt-col"},[_c('rt-radio-button',{attrs:{"is-disabled":true,"name":"type"}},[_vm._v("\n            Огонь\n          ")]),_vm._v(" "),_c('rt-radio-button',{attrs:{"is-disabled":true,"name":"type"}},[_vm._v("\n            Вода\n          ")]),_vm._v(" "),_c('rt-radio-button',{attrs:{"is-disabled":true,"name":"type"}},[_vm._v("\n            Медные трубы\n          ")])],1)]),_vm._v(" "),_c('div',{staticClass:"app-row row"},[_c('div',{staticClass:"rt-col"},[_c('rt-radio-button',{attrs:{"name":"type1"}},[_vm._v("\n            Огонь\n          ")]),_vm._v(" "),_c('rt-radio-button',{attrs:{"name":"type1"}},[_vm._v("\n            Вода\n          ")]),_vm._v(" "),_c('rt-radio-button',{attrs:{"name":"type1"}},[_vm._v("\n            Медные трубы\n          ")])],1)]),_vm._v(" "),_c('div',{staticClass:"app-row row rt-space-top"},[_c('documentation-builder',{attrs:{"json":_vm.documentation,"type":"components"}})],1)],1)])])}
-var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"app-title"},[_c('h1',{staticClass:"rt-font-hero"},[_vm._v("\n        RadioButton\n      ")])])},function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"app-row"},[_c('p',{staticClass:"rt-font-h3"},[_vm._v("\n          radiobutton\n        ")])])}]
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('div',{staticClass:"rt-container"},[_c('div',{staticClass:"app-content rt-col-12 rt-col-md-3"},[_vm._m(0),_vm._v(" "),_c('div',{staticClass:"app-body"},[_c('rt-row',[_c('rt-col',{attrs:{"size":6,"mobile-size":3}},[_c('pre-code',{attrs:{"text":"<div class=\"app-row row rt-space-top2\"><div class=\"rt-col\">\n\t\t\t\t\t<rt-radio-button name=\"type\" :checked=\"true\" value=\"0\">Вариант 1</rt-radio-button>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t<rt-radio-button name=\"type\" value=\"1\">Вариант 2</rt-radio-button></div>\n\t\t\t\t\t<rt-radio-button name=\"type\" value=\"2\">Вариант 3</rt-radio-button>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t  <rt-radio-button name=\"type\" value=\"3\">Вариант 4</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t  <rt-radio-button :has-error=\"true\" name=\"type\" value=\"4\">Вариант с ошибкой</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div>\n\t\t\t\t\t  <rt-radio-button :disabled=\"true\" :has-error=\"true\" name=\"type\" value=\"4\">Вариант неактивный</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"sp-t-1\">\n\t\t\t\t\t  <rt-radio-button :checked=\"true\" :disabled=\"true\" :has-error=\"true\" name=\"type\" value=\"4\">Вариант неактивный</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t</div></div>"}})],1),_vm._v(" "),_c('rt-col',{attrs:{"size":6,"mobile-size":3}},[_c('pre-code',{attrs:{"text":"<div class=\"app-row row rt-space-top2\"><div class=\"rt-col\">\n\t\t\t\t\t<rt-radio-button :is-orange=\"true\" name=\"type-2\" :checked=\"true\" value=\"0\">Вариант 1</rt-radio-button>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t<rt-radio-button :is-orange=\"true\" name=\"type-2\" value=\"1\">Вариант 2</rt-radio-button></div>\n\t\t\t\t\t<rt-radio-button :is-orange=\"true\" name=\"type-2\" value=\"2\">Вариант 3</rt-radio-button>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t  <rt-radio-button :is-orange=\"true\" name=\"type-2\" value=\"3\">Вариант 4</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t  <rt-radio-button :is-orange=\"true\" :has-error=\"true\" name=\"type-2\" value=\"4\">Вариант с ошибкой</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div>\n\t\t\t\t\t  <rt-radio-button :disabled=\"true\" :is-orange=\"true\" :has-error=\"true\" name=\"type-2\" value=\"4\">Вариант неактивный</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"sp-t-1\">\n\t\t\t\t\t  <rt-radio-button :checked=\"true\" :disabled=\"true\" :is-orange=\"true\" :has-error=\"true\" name=\"type-2\" value=\"4\">Вариант неактивный</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t</div></div>"}})],1)],1)],1)])]),_vm._v(" "),_c('div',{staticClass:"color-block--dark-blue rt-space-vertical2"},[_c('div',{staticClass:"rt-container"},[_c('rt-col',{attrs:{"size":12}},[_c('rt-row',[_c('rt-col',{attrs:{"size":6,"mobile-size":3}},[_c('pre-code',{attrs:{"text":"<div class=\"app-row row rt-space-top2\"><div class=\"rt-col\">\n\t\t\t\t\t<rt-radio-button name=\"type-3\" :bright=\"true\" :checked=\"true\" value=\"0\">Вариант 1</rt-radio-button>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t<rt-radio-button name=\"type-3\" :bright=\"true\" value=\"1\">Вариант 2</rt-radio-button></div>\n\t\t\t\t\t<rt-radio-button name=\"type-3\" :bright=\"true\" value=\"2\">Вариант 3</rt-radio-button>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t  <rt-radio-button name=\"type-3\" :bright=\"true\" value=\"3\">Вариант 4</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t  <rt-radio-button  :bright=\"true\" :has-error=\"true\" name=\"type-3\" value=\"4\">Вариант с ошибкой</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div>\n\t\t\t\t\t  <rt-radio-button :disabled=\"true\"  :bright=\"true\" :has-error=\"true\" name=\"type-3\" value=\"4\">Вариант неактивный</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"sp-t-1\">\n\t\t\t\t\t  <rt-radio-button :checked=\"true\" :disabled=\"true\"  :bright=\"true\" :has-error=\"true\" name=\"type-3\" value=\"4\">Вариант неактивный</rt-radio-button>\n\t\t\t\t\t</div></div>\n\t\t\t\t\t</div>"}})],1),_vm._v(" "),_c('rt-col',{attrs:{"size":6,"mobile-size":3}},[_c('pre-code',{attrs:{"text":"<div class=\"app-row row rt-space-top2\"><div class=\"rt-col\">\n\t\t\t\t\t<rt-radio-button :is-orange=\"true\" name=\"type-4\" :bright=\"true\" :checked=\"true\" value=\"0\">Вариант 1</rt-radio-button>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t<rt-radio-button :is-orange=\"true\" name=\"type-4\" :bright=\"true\" value=\"1\">Вариант 2</rt-radio-button></div>\n\t\t\t\t\t<rt-radio-button :is-orange=\"true\" name=\"type-4\" :bright=\"true\" value=\"2\">Вариант 3</rt-radio-button>\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t  <rt-radio-button :is-orange=\"true\" name=\"type-4\" :bright=\"true\" value=\"3\">Вариант 4</rt-radio-button>\n\t\t\t\t\t</div>\n\n\t\t\t\t\t<div class=\"rt-space-vertical\">\n\t\t\t\t\t  <rt-radio-button :is-orange=\"true\" :bright=\"true\" :has-error=\"true\" name=\"type-4\" value=\"4\">Вариант с ошибкой</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div>\n\t\t\t\t\t  <rt-radio-button :disabled=\"true\" :is-orange=\"true\" :bright=\"true\" :has-error=\"true\" name=\"type-4\" value=\"4\" >Вариант неактивный</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"sp-t-1\">\n\t\t\t\t\t  <rt-radio-button :checked=\"true\" :disabled=\"true\" :is-orange=\"true\" :bright=\"true\" :has-error=\"true\" name=\"type-4\" value=\"4\">Вариант неактивный</rt-radio-button>\n\t\t\t\t\t</div>\n\t\t\t\t\t</div></div>"}})],1)],1)],1)],1)]),_vm._v(" "),_c('div',{staticClass:"rt-container"},[_c('rt-row',{staticClass:"app-row rt-sp-t-1"},[_c('documentation-builder',{attrs:{"json":_vm.documentation,"type":"components"}})],1)],1)])}
+var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"app-title"},[_c('h1',{staticClass:"rt-font-hero"},[_vm._v("\n\t\t\t\t\tRadioButton\n\t\t\t\t")])])}]
 
 
 
@@ -115,7 +179,7 @@ var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _
 /*! exports provided: items, default */
 /***/ (function(module) {
 
-module.exports = JSON.parse("{\"items\":[{\"name\":\"rt-radio-button\",\"property\":\"name\",\"type\":\"String\",\"description\":\"Задаёт переключателю атрибут `name`\",\"value\":\"0\"},{\"name\":\"rt-radio-button\",\"property\":\":checked\",\"type\":\"Boolean\",\"description\":\"Предустанавливает выбранное состояние переключателю (атрибут `checked`)\",\"value\":\"false\"},{\"name\":\"rt-radio-button\",\"property\":\"value\",\"type\":\"String | Number\",\"description\":\"Задаёт переключателю атрибут `value`\",\"value\":\"null\"},{\"name\":\"rt-radio-button\",\"property\":\":is-disabled\",\"type\":\"Boolean\",\"description\":\"Флаг установки неактивного состояния для переключателя\",\"value\":\"false\"}]}");
+module.exports = JSON.parse("{\"items\":[{\"name\":\"rt-radio-button\",\"property\":\"name\",\"type\":\"String\",\"description\":\"Задаёт переключателю атрибут `name`\",\"value\":\"0\"},{\"name\":\"rt-radio-button\",\"property\":\":is-orange\",\"type\":\"Boolean\",\"description\":\"Делает чекбокс ораньжевым\",\"value\":\"false\",\"version\":\"0.3.24\"},{\"name\":\"rt-radio-button\",\"property\":\":bright\",\"type\":\"Boolean\",\"description\":\"Отображает чекбокс как в темной теме\",\"value\":\"false\",\"version\":\"0.3.24\"},{\"name\":\"rt-radio-button\",\"property\":\":checked\",\"type\":\"Boolean\",\"description\":\"Предустанавливает выбранное состояние переключателю (атрибут `checked`)\",\"value\":\"false\"},{\"name\":\"rt-radio-button\",\"property\":\"value\",\"type\":\"String | Number\",\"description\":\"Задаёт переключателю атрибут `value`\",\"value\":\"null\"},{\"name\":\"rt-radio-button\",\"property\":\":disabled\",\"type\":\"Boolean\",\"description\":\"Флаг установки неактивного состояния для переключателя\",\"value\":\"false\"}]}");
 
 /***/ }),
 
@@ -128,7 +192,7 @@ module.exports = JSON.parse("{\"items\":[{\"name\":\"rt-radio-button\",\"propert
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _Radiobutton_vue_vue_type_template_id_4171d250___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Radiobutton.vue?vue&type=template&id=4171d250& */ "./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=4171d250&");
+/* harmony import */ var _Radiobutton_vue_vue_type_template_id_1409d5f8___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Radiobutton.vue?vue&type=template&id=1409d5f8& */ "./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=1409d5f8&");
 /* harmony import */ var _Radiobutton_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Radiobutton.vue?vue&type=script&lang=js& */ "./src/example-pages/components/form/Radiobutton.vue?vue&type=script&lang=js&");
 /* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
@@ -140,8 +204,8 @@ __webpack_require__.r(__webpack_exports__);
 
 var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
   _Radiobutton_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
-  _Radiobutton_vue_vue_type_template_id_4171d250___WEBPACK_IMPORTED_MODULE_0__["render"],
-  _Radiobutton_vue_vue_type_template_id_4171d250___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  _Radiobutton_vue_vue_type_template_id_1409d5f8___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _Radiobutton_vue_vue_type_template_id_1409d5f8___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
   null,
   null,
@@ -167,19 +231,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=4171d250&":
+/***/ "./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=1409d5f8&":
 /*!******************************************************************************************!*\
-  !*** ./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=4171d250& ***!
+  !*** ./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=1409d5f8& ***!
   \******************************************************************************************/
 /*! exports provided: render, staticRenderFns */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Radiobutton_vue_vue_type_template_id_4171d250___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./Radiobutton.vue?vue&type=template&id=4171d250& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=4171d250&");
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Radiobutton_vue_vue_type_template_id_4171d250___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Radiobutton_vue_vue_type_template_id_1409d5f8___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./Radiobutton.vue?vue&type=template&id=1409d5f8& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/form/Radiobutton.vue?vue&type=template&id=1409d5f8&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Radiobutton_vue_vue_type_template_id_1409d5f8___WEBPACK_IMPORTED_MODULE_0__["render"]; });
 
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Radiobutton_vue_vue_type_template_id_4171d250___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Radiobutton_vue_vue_type_template_id_1409d5f8___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
 
 
 
--- src/example-pages/components/form/RadioButton.vue
+++ src/example-pages/components/form/RadioButton.vue
@@ -1,54 +1,122 @@
 <template>
-  <div class="rt-container">
-    <div class="app-content rt-col-12 rt-col-md-3">
-      <div class="app-title">
-        <h1 class="rt-font-hero">
-          RadioButton
-        </h1>
-      </div>
-      <div class="app-body">
-        <div class="app-row">
-          <p class="rt-font-h3">
-            radiobutton
-          </p>
-        </div>
-        <div class="rt-space-bottom2">testModel = {{ testModel }}</div>
+	<div>
+		<div class="rt-container">
+			<div class="app-content rt-col-12 rt-col-md-3">
+				<div class="app-title">
+					<h1 class="rt-font-hero">
+						RadioButton
+					</h1>
+				</div>
+				<div class="app-body">
+					<rt-row>
+						<rt-col :size="6" :mobile-size="3">
+							<pre-code
+									text='<div class="app-row row rt-space-top2"><div class="rt-col">
+						<rt-radio-button name="type" :checked="true" value="0">Вариант 1</rt-radio-button>
+						<div class="rt-space-vertical">
+						<rt-radio-button name="type" value="1">Вариант 2</rt-radio-button></div>
+						<rt-radio-button name="type" value="2">Вариант 3</rt-radio-button>
+						<div class="rt-space-vertical">
+						  <rt-radio-button name="type" value="3">Вариант 4</rt-radio-button>
+						</div>
+						<div class="rt-space-vertical">
+						  <rt-radio-button :has-error="true" name="type" value="4">Вариант с ошибкой</rt-radio-button>
+						</div>
+						<div>
+						  <rt-radio-button :disabled="true" :has-error="true" name="type" value="4">Вариант неактивный</rt-radio-button>
+						</div>
+						<div class="sp-t-1">
+						  <rt-radio-button :checked="true" :disabled="true" :has-error="true" name="type" value="4">Вариант неактивный</rt-radio-button>
+						</div>
+						</div></div>'
+							/>
+						</rt-col>
+						<rt-col :size="6" :mobile-size="3">
+							<pre-code
+									text='<div class="app-row row rt-space-top2"><div class="rt-col">
+						<rt-radio-button :is-orange="true" name="type-2" :checked="true" value="0">Вариант 1</rt-radio-button>
+						<div class="rt-space-vertical">
+						<rt-radio-button :is-orange="true" name="type-2" value="1">Вариант 2</rt-radio-button></div>
+						<rt-radio-button :is-orange="true" name="type-2" value="2">Вариант 3</rt-radio-button>
+						<div class="rt-space-vertical">
+						  <rt-radio-button :is-orange="true" name="type-2" value="3">Вариант 4</rt-radio-button>
+						</div>
+						<div class="rt-space-vertical">
+						  <rt-radio-button :is-orange="true" :has-error="true" name="type-2" value="4">Вариант с ошибкой</rt-radio-button>
+						</div>
+						<div>
+						  <rt-radio-button :disabled="true" :is-orange="true" :has-error="true" name="type-2" value="4">Вариант неактивный</rt-radio-button>
+						</div>
+						<div class="sp-t-1">
+						  <rt-radio-button :checked="true" :disabled="true" :is-orange="true" :has-error="true" name="type-2" value="4">Вариант неактивный</rt-radio-button>
+						</div>
+						</div></div>'
+							/>
+						</rt-col>
+					</rt-row>
 
-        <pre-code
-          text="<div class=&quot;app-row row rt-space-top2&quot;><div class=&quot;rt-col&quot;><rt-radio-button name=&quot;type&quot; @change=&quot;setTestModel&quot; value=&quot;0&quot; :model=&quot;testModel&quot;>Огонь</rt-radio-button><div class=&quot;rt-space-vertical&quot;><rt-radio-button name=&quot;type&quot; @change=&quot;setTestModel&quot; value=&quot;1&quot; :model=&quot;testModel&quot;>Вода</rt-radio-button></div><rt-radio-button name=&quot;type&quot; @change=&quot;setTestModel&quot; value=&quot;2&quot; :model=&quot;testModel&quot;>Медные трубы</rt-radio-button></div></div>"
-        />
-        <div class="app-row row">
-          <div class="rt-col">
-            <rt-radio-button :is-disabled="true" name="type">
-              Огонь
-            </rt-radio-button>
-            <rt-radio-button :is-disabled="true" name="type">
-              Вода
-            </rt-radio-button>
-            <rt-radio-button :is-disabled="true" name="type">
-              Медные трубы
-            </rt-radio-button>
-          </div>
-        </div>
-        <div class="app-row row">
-          <div class="rt-col">
-            <rt-radio-button name="type1">
-              Огонь
-            </rt-radio-button>
-            <rt-radio-button name="type1">
-              Вода
-            </rt-radio-button>
-            <rt-radio-button name="type1">
-              Медные трубы
-            </rt-radio-button>
-          </div>
-        </div>
-        <div class="app-row row rt-space-top">
-          <documentation-builder :json="documentation" type="components"/>
-        </div>
-      </div>
-    </div>
-  </div>
+				</div>
+			</div>
+		</div>
+		<div class="color-block--dark-blue rt-space-vertical2">
+			<div class="rt-container">
+				<rt-col :size="12">
+					<rt-row>
+						<rt-col :size="6" :mobile-size="3">
+							<pre-code
+									text='<div class="app-row row rt-space-top2"><div class="rt-col">
+						<rt-radio-button name="type-3" :bright="true" :checked="true" value="0">Вариант 1</rt-radio-button>
+						<div class="rt-space-vertical">
+						<rt-radio-button name="type-3" :bright="true" value="1">Вариант 2</rt-radio-button></div>
+						<rt-radio-button name="type-3" :bright="true" value="2">Вариант 3</rt-radio-button>
+						<div class="rt-space-vertical">
+						  <rt-radio-button name="type-3" :bright="true" value="3">Вариант 4</rt-radio-button>
+						</div>
+						<div class="rt-space-vertical">
+						  <rt-radio-button  :bright="true" :has-error="true" name="type-3" value="4">Вариант с ошибкой</rt-radio-button>
+						</div>
+						<div>
+						  <rt-radio-button :disabled="true"  :bright="true" :has-error="true" name="type-3" value="4">Вариант неактивный</rt-radio-button>
+						</div>
+						<div class="sp-t-1">
+						  <rt-radio-button :checked="true" :disabled="true"  :bright="true" :has-error="true" name="type-3" value="4">Вариант неактивный</rt-radio-button>
+						</div></div>
+						</div>'
+							/>
+						</rt-col>
+						<rt-col :size="6" :mobile-size="3">
+							<pre-code
+									text='<div class="app-row row rt-space-top2"><div class="rt-col">
+						<rt-radio-button :is-orange="true" name="type-4" :bright="true" :checked="true" value="0">Вариант 1</rt-radio-button>
+						<div class="rt-space-vertical">
+						<rt-radio-button :is-orange="true" name="type-4" :bright="true" value="1">Вариант 2</rt-radio-button></div>
+						<rt-radio-button :is-orange="true" name="type-4" :bright="true" value="2">Вариант 3</rt-radio-button>
+						<div class="rt-space-vertical">
+						  <rt-radio-button :is-orange="true" name="type-4" :bright="true" value="3">Вариант 4</rt-radio-button>
+						</div>
+
+						<div class="rt-space-vertical">
+						  <rt-radio-button :is-orange="true" :bright="true" :has-error="true" name="type-4" value="4">Вариант с ошибкой</rt-radio-button>
+						</div>
+						<div>
+						  <rt-radio-button :disabled="true" :is-orange="true" :bright="true" :has-error="true" name="type-4" value="4" >Вариант неактивный</rt-radio-button>
+						</div>
+						<div class="sp-t-1">
+						  <rt-radio-button :checked="true" :disabled="true" :is-orange="true" :bright="true" :has-error="true" name="type-4" value="4">Вариант неактивный</rt-radio-button>
+						</div>
+						</div></div>'
+							/>
+						</rt-col>
+					</rt-row>
+				</rt-col>
+			</div>
+		</div>
+		<div class="rt-container">
+			<rt-row class="app-row rt-sp-t-1">
+				<documentation-builder :json="documentation" type="components"/>
+			</rt-row>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -67,11 +135,8 @@
     mounted() {
       this.documentation = documentation;
     },
-    created() {},
-    methods: {
-      setTestModel(value) {
-        this.testModel = value;
-      }
-    }
-};
+    created() {
+    },
+    methods: {}
+  };
 </script>
