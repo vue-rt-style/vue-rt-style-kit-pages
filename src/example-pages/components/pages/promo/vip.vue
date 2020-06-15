bdc9824df617e7df04dc448ca8a7dd6c2be99378
cryingjoker <cryingjoker@me.com>
2020-06-15 20:24:43 +0300

    update game promo

--- src/example-pages/components/pages/promo/vip.vue
+++ src/example-pages/components/pages/promo/vip.vue
@@ -3,14 +3,14 @@
 		<div class="cloud-g-1 d-flex flex-center-center">
 			<div class="rt-container">
 				<rt-col size="12">
-					<rt-row>
-						<rt-col size="5">
-							<h1 class="rt-font-h1 rt-font-light">Подключайте <br> Облачные игры NVIDIA <br> GeForce NOW</h1>
-							<p class="sp-t-1-3 sp-b-2">Наслаждайтесь любимыми играми на максимальных <br> настройках графики, даже на
+					<rt-row class="row-td-reverse">
+						<rt-col size="5" tablet-size="6">
+							<h1 class="rt-font-h1 rt-font-light td-sp-t-0-3">Подключайте <br class="td-d-none"> Облачные игры NVIDIA <br> GeForce NOW</h1>
+							<p class="sp-t-1-3 td-sp-t-0-3 td-sp-b-1 sp-b-2 color-white07">Наслаждайтесь любимыми играми на максимальных <br> настройках графики, даже на
 								слабом компьютере</p>
 							<rt-button class="rt-button-full-white">Выбрать тариф</rt-button>
 						</rt-col>
-						<rt-col size="7" class="d-flex flex-center-center">
+						<rt-col size="7" tablet-size="6" class="d-flex flex-center-center">
 							<div class="cloud-g-1-image"></div>
 						</rt-col>
 					</rt-row>
@@ -18,7 +18,7 @@
 			</div>
 		</div>
 
-		<div class="cloud-g-2 sp-t-1-4">
+		<div class="cloud-g-2 sp-t-1-4 td-sp-t-3-1 md-sp-t-2">
 			<div class="rt-container">
 				<rt-col>
 					<h3 class="rt-font-h3 rt-font-light sp-b-0-1">Играйте в топовые игры</h3>
@@ -26,7 +26,7 @@
 			</div>
 
 			<div class="cloud-g-slider sp-v-1-4">
-				<div class="rt-container cloud-g-slider-arrow-container">
+				<div class="rt-container cloud-g-slider-arrow-container td-d-none">
 					<div class="cloud-g-slider-arrow cloud-g-slider-arrow-left">
 						<svg width="51px" height="52px" viewBox="0 0 51 52" version="1.1" xmlns="http://www.w3.org/2000/svg">
 							<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -158,44 +158,44 @@
 			</div>
 		</div>
 		<div class="cloud-g-bg-image">
-			<div class="cloud-g-3 sp-t-3-1">
+			<div class="cloud-g-3 sp-t-3-1 td-sp-t-1-1 md-sp-t-0-1">
 				<div class="rt-container">
 					<rt-col>
 						<h3 class="rt-font-h3 rt-font-light sp-b-2">Забудьте об ограничениях</h3>
-						<rt-row>
-							<rt-col size="3">
+						<rt-row class="row--tablet-scroll">
+							<rt-col size="3" tablet-size="2" mobile-size="2">
 								<img src="https://static-cdn.rt.ru/sites/default/files/cloud-game/cloud-game-icon1.png"
-								     class="ratio2x-d-none" alt="">
+								     class="ratio2x-d-none cloud-g-icon" alt="">
 								<img src="https://static-cdn.rt.ru/sites/default/files/cloud-game/cloud-game-icon1-lg.png"
-								     class="ratio2x-d-block d-none" alt="">
+								     class="ratio2x-d-block d-none cloud-g-icon" alt="">
 								<p class="rt-font-paragraph rt-font-bold sp-t-1">Играйте на любом устройстве</p>
 								<p class="rt-font-small-paragraph sp-t-0-3 color-white07">Запускайте топовые игры на старом ПК,
 									новеньком MacBook,
 									ТВ-приставке или даже телефоне на Android OS</p>
 							</rt-col>
-							<rt-col size="3">
+							<rt-col size="3" tablet-size="2" mobile-size="2">
 								<img src="https://static-cdn.rt.ru/sites/default/files/cloud-game/cloud-game-icon2.png"
-								     class="ratio2x-d-none" alt="">
+								     class="ratio2x-d-none cloud-g-icon" alt="">
 								<img src="https://static-cdn.rt.ru/sites/default/files/cloud-game/cloud-game-icon2-lg.png"
-								     class="ratio2x-d-block d-none" alt="">
+								     class="ratio2x-d-block d-none cloud-g-icon" alt="">
 								<p class="rt-font-paragraph rt-font-bold sp-t-1">Не тратьте время на подготовку</p>
 								<p class="rt-font-small-paragraph sp-t-0-3 color-white07">Все патчи и обновления уже загружены</p>
 							</rt-col>
-							<rt-col size="3">
+							<rt-col size="3" tablet-size="2" mobile-size="2">
 								<img src="https://static-cdn.rt.ru/sites/default/files/cloud-game/cloud-game-icon3.png"
-								     class="ratio2x-d-none" alt="">
+								     class="ratio2x-d-none cloud-g-icon" alt="">
 								<img src="https://static-cdn.rt.ru/sites/default/files/cloud-game/cloud-game-icon3-lg.png"
-								     class="ratio2x-d-block d-none" alt="">
+								     class="ratio2x-d-block d-none cloud-g-icon" alt="">
 								<p class="rt-font-paragraph rt-font-bold sp-t-1">Наслаждайтесь фотореалистичной графикой</p>
 								<p class="rt-font-small-paragraph sp-t-0-3 color-white07">Максимально реалистичная картинка с
 									технологиями NVIDIA
 									GeForce RTX для полного погружения в игру</p>
 							</rt-col>
-							<rt-col size="3">
+							<rt-col size="3" tablet-size="2" mobile-size="2">
 								<img src="https://static-cdn.rt.ru/sites/default/files/cloud-game/cloud-game-icon4.png"
-								     class="ratio2x-d-none" alt="">
+								     class="ratio2x-d-none cloud-g-icon" alt="">
 								<img src="https://static-cdn.rt.ru/sites/default/files/cloud-game/cloud-game-icon4-lg.png"
-								     class="ratio2x-d-block d-none" alt="">
+								     class="ratio2x-d-block d-none cloud-g-icon" alt="">
 								<p class="rt-font-paragraph rt-font-bold sp-t-1">Разгрузите свой жёсткий диск </p>
 								<p class="rt-font-small-paragraph sp-t-0-3 color-white07">Запускайте игры в облаке и не забивайте свой
 									диск гигабайтами
@@ -209,37 +209,188 @@
 				<div class="rt-container">
 
 					<rt-col>
-						<h3 class="rt-font-h3 rt-font-light sp-b-2">Подключайтесь в личном кабинете</h3>
+						<h3 class="rt-font-h3 rt-font-light sp-b-2">Подключайтесь в личном кабинете
+
+
+						</h3>
 						<rt-row>
 							<rt-col size="6">
 								<rt-card background-color-type="black" :is-white-color='true' class="rt-cart-cloud-g">
-									<template slot='content'><p class='rt-font-paragraph rt-font-bold'>Мобильное ТВ</p>
-										<p class='rt-space-top05'>101 тв-канал с контентом на любой вкус в подарок </p></template>
-									<template slot='footer'><p>Подробнее</p></template>
+									<template slot='content'><p class='rt-font-h3 rt-font-bold sp-b-0-1'>На тарифе «Игровой»</p>
+										<p class='color-white07 sp-b-1'>При условии отсутсвия активных игровых опций</p>
+										<ul class="cloud-g-list">
+											<li class="cloud-g-list-item rt-font-paragraph">Премиальный доступ без ограничений</li>
+											<li class="cloud-g-list-item rt-font-paragraph">Технологии трассировки лучей RTX</li>
+											<li class="cloud-g-list-item rt-font-paragraph">Высокий приоритет очереди</li>
+										</ul>
+									</template>
+									<template slot='footer'>
+										<div class="d-flex">
+											<div class="flex-fill">
+												<rt-price
+														:value='490'
+												></rt-price>
+											</div>
+											<div class="flex-end-center">
+												<rt-button href="#" class="rt-button-full-white">Подключить</rt-button>
+											</div>
+
+										</div>
+
+									</template>
 								</rt-card>
 							</rt-col>
 							<rt-col size="6">
 								<rt-card background-color-type="black" :is-white-color='true' class="rt-cart-cloud-g">
-									<template slot='content'><p class='rt-font-paragraph rt-font-bold'>Мобильное ТВ</p>
-										<p class='rt-space-top05'>101 тв-канал с контентом на любой вкус в подарок </p></template>
-									<template slot='footer'><p>Подробнее</p></template>
+									<template slot='content'><p class='rt-font-h3 rt-font-bold sp-b-0-1'>На любом тарифе</p>
+										<p class='color-white07 sp-b-1'>&nbsp;</p>
+										<ul class="cloud-g-list">
+											<li class="cloud-g-list-item rt-font-paragraph">Премиальный доступ без ограничений</li>
+											<li class="cloud-g-list-item rt-font-paragraph">Технологии трассировки лучей RTX</li>
+											<li class="cloud-g-list-item rt-font-paragraph">Высокий приоритет очереди</li>
+										</ul>
+									</template>
+									<template slot='footer'>
+										<div class="d-flex">
+											<div class="flex-fill">
+												<rt-price
+														:value='999'
+												></rt-price>
+											</div>
+											<div class="flex-end-center">
+												<rt-button href="#" class="rt-button-full-white">Подключить</rt-button>
+											</div>
+
+										</div>
+
+									</template>
 								</rt-card>
 							</rt-col>
 						</rt-row>
 					</rt-col>
 				</div>
 			</div>
+
 		</div>
-		<div class="sp-t-5 sp-b-5">
-			<p>
-				123лдвлфыджлвждыфлжвдлфы
-				влфыд
-				лвд
-				флы
-				вдлфы
-				двл
-				дфы
-			</p>
+		<div class="sp-t-5">
+			<div class="rt-container">
+				<rt-col>
+					<h3 class="rt-font-h3">Вы не клиент Ростелекома? </h3>
+					<p class="rt-font-paragraph sp-t-2-2 sp-b-2-3">Отправьте заявку на подключения тарифа «Игровой» </p>
+					<rt-button class="rt-button-white">Отправить заявку</rt-button>
+				</rt-col>
+			</div>
+		</div>
+		<div class="sp-t-5">
+			<div class="rt-container">
+				<rt-col>
+					<h3 class="rt-font-h3 sp-b-2">Популярные вопросы</h3>
+					<rt-slide-content>
+						<rt-template slot="label">
+							Как подключить услугу «Облачные игры от gfn.ru»?
+						</rt-template>
+						<rt-template slot="content">
+							Как подключить услугу «Облачные игры от gfn.ru»?
+							Как подключить услугу «Облачные игры от gfn.ru»?
+							Как подключить услугу «Облачные игры от gfn.ru»?
+							Как подключить услугу «Облачные игры от gfn.ru»?
+						</rt-template>
+					</rt-slide-content>
+					<rt-slide-content>
+						<rt-template slot="label">
+							Какие минимальные системные требования?
+						</rt-template>
+						<rt-template slot="content">
+							Как подключить услугу «Облачные игры от gfn.ru»?
+							Как подключить услугу «Облачные игры от gfn.ru»?
+							Как подключить услугу «Облачные игры от gfn.ru»?
+							Как подключить услугу «Облачные игры от gfn.ru»?
+						</rt-template>
+					</rt-slide-content>
+					<rt-slide-content>
+						<rt-template slot="label">
+							Как обеспечить подключение?
+						</rt-template>
+						<rt-template slot="content">
+							Как подключить услугу «Облачные игры от gfn.ru»?
+							Как подключить услугу «Облачные игры от gfn.ru»?
+							Как подключить услугу «Облачные игры от gfn.ru»?
+							Как подключить услугу «Облачные игры от gfn.ru»?
+						</rt-template>
+					</rt-slide-content>
+				</rt-col>
+			</div>
+		</div>
+		<div class="sp-t-5 sp-b-3">
+			<div class="rt-container">
+				<rt-col>
+					<h3 class="rt-font-h3 sp-b-2">Полезные материалы</h3>
+				</rt-col>
+				<rt-col>
+					<rt-row class="row--mobile-scroll">
+						<rt-col size="4" table-size="2" mobile-size="2">
+							<rt-card background-color-type="black" :is-white-color='true' class="rt-cart-cloud-g">
+								<template slot='content'>
+									<p class='rt-font-paragraph sp-b-1'>Условия предоставления услуги</p>
+									<a href="">
+										<svg width="16px" height="18px" viewBox="0 0 16 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+											<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+												<g id="RTRU-7499_Documentation" transform="translate(-1088.000000, -1888.000000)">
+													<g id="Group" transform="translate(1088.000000, 1888.000000)" fill="#5FBD00">
+														<g id="icons-/-service-/-download-/-download-24">
+															<path d="M14.25,15.7505 C14.803,15.7505 15.25,16.1985 15.25,16.7505 C15.25,17.3025 14.803,17.7505 14.25,17.7505 L14.25,17.7505 L1.75,17.7505 C1.197,17.7505 0.75,17.3025 0.75,16.7505 C0.75,16.1985 1.197,15.7505 1.75,15.7505 L1.75,15.7505 Z M8.0004,0.25 C8.5524,0.25 9.0004,0.698 9.0004,1.25 L9.0004,1.25 L9.0004,9.735 L11.5234,7.064 C11.9024,6.663 12.5344,6.644 12.9364,7.023 C13.3374,7.403 13.3564,8.036 12.9764,8.437 L12.9764,8.437 L8.7264,12.937 C8.5384,13.137 8.2754,13.25 8.0004,13.25 C7.7244,13.25 7.4614,13.137 7.2734,12.937 L7.2734,12.937 L3.0234,8.437 C2.6434,8.036 2.6624,7.403 3.0634,7.023 C3.4654,6.644 4.0984,6.663 4.4764,7.064 L4.4764,7.064 L7.0004,9.735 L7.0004,1.25 C7.0004,0.698 7.4474,0.25 8.0004,0.25 Z" id="Fill-5"></path>
+														</g>
+													</g>
+												</g>
+											</g>
+										</svg>
+										<span class="sp-l-0-2 rt-cart-cloud-g-link">Скачать PDF</span></a>
+								</template>
+							</rt-card>
+						</rt-col>
+						<rt-col size="4" table-size="2" mobile-size="2">
+							<rt-card background-color-type="black" :is-white-color='true' class="rt-cart-cloud-g">
+								<template slot='content'>
+									<p class='rt-font-paragraph sp-b-1'>Системные требования</p>
+									<a href="">
+										<svg width="16px" height="18px" viewBox="0 0 16 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+											<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+												<g id="RTRU-7499_Documentation" transform="translate(-1088.000000, -1888.000000)">
+													<g id="Group" transform="translate(1088.000000, 1888.000000)" fill="#5FBD00">
+														<g id="icons-/-service-/-download-/-download-24">
+															<path d="M14.25,15.7505 C14.803,15.7505 15.25,16.1985 15.25,16.7505 C15.25,17.3025 14.803,17.7505 14.25,17.7505 L14.25,17.7505 L1.75,17.7505 C1.197,17.7505 0.75,17.3025 0.75,16.7505 C0.75,16.1985 1.197,15.7505 1.75,15.7505 L1.75,15.7505 Z M8.0004,0.25 C8.5524,0.25 9.0004,0.698 9.0004,1.25 L9.0004,1.25 L9.0004,9.735 L11.5234,7.064 C11.9024,6.663 12.5344,6.644 12.9364,7.023 C13.3374,7.403 13.3564,8.036 12.9764,8.437 L12.9764,8.437 L8.7264,12.937 C8.5384,13.137 8.2754,13.25 8.0004,13.25 C7.7244,13.25 7.4614,13.137 7.2734,12.937 L7.2734,12.937 L3.0234,8.437 C2.6434,8.036 2.6624,7.403 3.0634,7.023 C3.4654,6.644 4.0984,6.663 4.4764,7.064 L4.4764,7.064 L7.0004,9.735 L7.0004,1.25 C7.0004,0.698 7.4474,0.25 8.0004,0.25 Z" id="Fill-5"></path>
+														</g>
+													</g>
+												</g>
+											</g>
+										</svg>
+										<span class="sp-l-0-2 rt-cart-cloud-g-link">Скачать PDF</span></a>
+								</template>
+							</rt-card>
+						</rt-col>
+						<rt-col size="4" table-size="2" mobile-size="2">
+							<rt-card background-color-type="black" :is-white-color='true' class="rt-cart-cloud-g">
+								<template slot='content'>
+									<p class='rt-font-paragraph sp-b-1'>Инструкция по подключению услуги</p>
+									<a href="">
+										<svg width="16px" height="18px" viewBox="0 0 16 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+											<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+												<g id="RTRU-7499_Documentation" transform="translate(-1088.000000, -1888.000000)">
+													<g id="Group" transform="translate(1088.000000, 1888.000000)" fill="#5FBD00">
+														<g id="icons-/-service-/-download-/-download-24">
+															<path d="M14.25,15.7505 C14.803,15.7505 15.25,16.1985 15.25,16.7505 C15.25,17.3025 14.803,17.7505 14.25,17.7505 L14.25,17.7505 L1.75,17.7505 C1.197,17.7505 0.75,17.3025 0.75,16.7505 C0.75,16.1985 1.197,15.7505 1.75,15.7505 L1.75,15.7505 Z M8.0004,0.25 C8.5524,0.25 9.0004,0.698 9.0004,1.25 L9.0004,1.25 L9.0004,9.735 L11.5234,7.064 C11.9024,6.663 12.5344,6.644 12.9364,7.023 C13.3374,7.403 13.3564,8.036 12.9764,8.437 L12.9764,8.437 L8.7264,12.937 C8.5384,13.137 8.2754,13.25 8.0004,13.25 C7.7244,13.25 7.4614,13.137 7.2734,12.937 L7.2734,12.937 L3.0234,8.437 C2.6434,8.036 2.6624,7.403 3.0634,7.023 C3.4654,6.644 4.0984,6.663 4.4764,7.064 L4.4764,7.064 L7.0004,9.735 L7.0004,1.25 C7.0004,0.698 7.4474,0.25 8.0004,0.25 Z" id="Fill-5"></path>
+														</g>
+													</g>
+												</g>
+											</g>
+										</svg>
+										<span class="sp-l-0-2 rt-cart-cloud-g-link">Скачать PDF</span></a>
+								</template>
+							</rt-card>
+						</rt-col>
+					</rt-row>
+				</rt-col>
+			</div>
 		</div>
 
 
@@ -264,10 +415,21 @@
       var isAnimated = false;
       var wrapper = document.querySelector('.cloud-g-slider-inner')
       var index = 0;
+      window.addEventListener('resize',function(){
+        var slideArray = document.querySelectorAll('.cloud-g-slider-item')
+        var step = slideArray[1].clientWidth
+        var slide = slideArray[0];
+        var marginLeft = step * index * -1;
+        if(window.innerWidth > 1024) {
+          slide.style.marginLeft = marginLeft + 'px';
+        }else{
+          slide.style.marginLeft = 0
+        }
+      });
       document.querySelectorAll('.cloud-g-slider-arrow').forEach(function (item) {
 
 				var setRigthArrow = function(){
-          console.info('index',index)
+
 				  var arrowR = document.querySelector('.cloud-g-slider-arrow-right')
 				  var arrowL = document.querySelector('.cloud-g-slider-arrow-left')
 				  if(index >= 0){
@@ -283,28 +445,24 @@
         item.addEventListener('click', function () {
           var slideArray = document.querySelectorAll('.cloud-g-slider-item')
           var slide = slideArray[0];
-          var step = 300
+          var step = slideArray[1].clientWidth
           if (!isAnimated) {
             isAnimated = true
             if (item.classList.contains('cloud-g-slider-arrow-left')) {
-              var marginLeft = parseInt(slide.style.marginLeft);
-              if(!marginLeft){
-                marginLeft = 0
-              }
-              marginLeft-=step
-              index= index - 1
+              index= index - 1;
+              var marginLeft =step*index;
 
-              slide.style.marginLeft = marginLeft+'px'
+              slide.style.marginLeft = marginLeft+'px';
               setTimeout(function () {
-                isAnimated = false
+                isAnimated = false;
                 setRigthArrow()
               }, 200)
             } else {
-              var marginLeft = parseInt(slide.style.marginLeft);
-              marginLeft+=step
-              index = index + 1
 
-              slide.style.marginLeft = marginLeft+'px'
+              index = index + 1;
+              var marginLeft =step*index;
+
+              slide.style.marginLeft = marginLeft+'px';
               setTimeout(function () {
                 isAnimated = false
                 setRigthArrow()
