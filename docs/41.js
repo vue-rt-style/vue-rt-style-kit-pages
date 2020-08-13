c739df88f292ea40e6bbff5039959e71be5c21fd
cryingjoker <cryingjoker@me.com>
2020-08-13 14:24:08 +0300

    deploy docs

--- docs/41.js
+++ docs/41.js
@@ -62,9 +62,9 @@ var componentsList = {};
 
 /***/ }),
 
-/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=6d7474da&":
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=339afaf8&":
 /*!************************************************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=6d7474da& ***!
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=339afaf8& ***!
   \************************************************************************************************************************************************************************************************************************************************/
 /*! exports provided: render, staticRenderFns */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
@@ -73,7 +73,7 @@ var componentsList = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"conference"},[_c('a-c-header',{attrs:{"phone":88006661313,"name":"Абракадабрович Мadfgsdfg"}}),_vm._v(" "),_c('div',{staticClass:"rt-container conference__content-wrapper"},[_c('div',{staticClass:"rt-col"},[_c('div',{staticClass:"row sp-t-2-3 sp-b-1-3 align-center"},[_vm._m(0),_vm._v(" "),_c('div',{staticClass:"rt-col-4 rt-font-right"},[_c('rt-button',{staticClass:"rt-button-orange-border color-orange"},[_vm._v("Добавить новый контакт")])],1),_vm._v(" "),_c('div',{staticClass:"rt-col-4"},[_c('rt-input',{attrs:{"type":"text","is-b2b-input":true,"outlined":true,"placeholder":"Поиск по контактам"}})],1)])]),_vm._v(" "),_c('div',{staticClass:"rt-col"},[_c('div',{staticClass:"sp-h-0-1 d-flex d-space-between sp-b-0-2"},[_c('button',{staticClass:"phonebook__letter"},[_vm._v("все")]),_vm._v(" "),_vm._l((_vm.arr_RU),function(letter){return _c('button',{staticClass:"phonebook__letter"},[_vm._v("\n                    "+_vm._s(letter)+"\n                ")])}),_vm._v(" "),_c('button',{staticClass:"phonebook__letter"},[_vm._v("A-Z")]),_vm._v(" "),_c('button',{staticClass:"phonebook__letter"},[_vm._v("0-9")])],2)]),_vm._v(" "),_c('div',{staticClass:"rt-col"},[_c('a-c-phonebook-component',{attrs:{"phone-book":[{'adf': 'adf'}]}})],1)]),_vm._v(" "),_c('a-c-footer')],1)}
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"conference"},[_c('a-c-header',{attrs:{"phone":88006661313,"name":"Абракадабрович Мadfgsdfg"}}),_vm._v(" "),_c('div',{staticClass:"rt-container conference__content-wrapper"},[_c('div',{staticClass:"rt-col"},[_c('div',{staticClass:"row sp-t-2-3 sp-b-1-3 flex-v-center"},[_vm._m(0),_vm._v(" "),_c('div',{staticClass:"rt-col-4 rt-font-right"},[_c('rt-button',{staticClass:"rt-button-orange-border color-orange"},[_vm._v("Добавить новый контакт")])],1),_vm._v(" "),_c('div',{staticClass:"rt-col-4"},[_c('rt-input',{attrs:{"type":"text","is-b2b-input":true,"outlined":true,"placeholder":"Поиск по контактам"}})],1)])]),_vm._v(" "),_c('div',{staticClass:"rt-col"},[_c('div',{staticClass:"sp-h-0-1 d-flex d-space-between sp-b-0-2"},[_c('button',{staticClass:"phonebook__letter"},[_vm._v("все")]),_vm._v(" "),_vm._l((_vm.arr_RU),function(letter){return _c('button',{staticClass:"phonebook__letter"},[_vm._v("\n                    "+_vm._s(letter)+"\n                ")])}),_vm._v(" "),_c('button',{staticClass:"phonebook__letter"},[_vm._v("A-Z")]),_vm._v(" "),_c('button',{staticClass:"phonebook__letter"},[_vm._v("0-9")])],2)]),_vm._v(" "),_c('div',{staticClass:"rt-col"},[_c('a-c-phonebook-component',{attrs:{"phone-book":[{'adf': 'adf'}]}})],1)]),_vm._v(" "),_c('a-c-footer')],1)}
 var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"rt-col-4"},[_c('h1',{staticClass:"rt-font-h2"},[_vm._v("Адресная книга")])])}]
 
 
@@ -89,7 +89,7 @@ var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _AudioConferencePhonebook_vue_vue_type_template_id_6d7474da___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./AudioConferencePhonebook.vue?vue&type=template&id=6d7474da& */ "./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=6d7474da&");
+/* harmony import */ var _AudioConferencePhonebook_vue_vue_type_template_id_339afaf8___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./AudioConferencePhonebook.vue?vue&type=template&id=339afaf8& */ "./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=339afaf8&");
 /* harmony import */ var _AudioConferencePhonebook_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./AudioConferencePhonebook.vue?vue&type=script&lang=js& */ "./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=script&lang=js&");
 /* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
@@ -101,8 +101,8 @@ __webpack_require__.r(__webpack_exports__);
 
 var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
   _AudioConferencePhonebook_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
-  _AudioConferencePhonebook_vue_vue_type_template_id_6d7474da___WEBPACK_IMPORTED_MODULE_0__["render"],
-  _AudioConferencePhonebook_vue_vue_type_template_id_6d7474da___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  _AudioConferencePhonebook_vue_vue_type_template_id_339afaf8___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _AudioConferencePhonebook_vue_vue_type_template_id_339afaf8___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
   null,
   null,
@@ -128,19 +128,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=6d7474da&":
+/***/ "./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=339afaf8&":
 /*!******************************************************************************************************************!*\
-  !*** ./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=6d7474da& ***!
+  !*** ./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=339afaf8& ***!
   \******************************************************************************************************************/
 /*! exports provided: render, staticRenderFns */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AudioConferencePhonebook_vue_vue_type_template_id_6d7474da___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./AudioConferencePhonebook.vue?vue&type=template&id=6d7474da& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=6d7474da&");
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AudioConferencePhonebook_vue_vue_type_template_id_6d7474da___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AudioConferencePhonebook_vue_vue_type_template_id_339afaf8___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./AudioConferencePhonebook.vue?vue&type=template&id=339afaf8& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/audioconference/AudioConferencePhonebook.vue?vue&type=template&id=339afaf8&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AudioConferencePhonebook_vue_vue_type_template_id_339afaf8___WEBPACK_IMPORTED_MODULE_0__["render"]; });
 
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AudioConferencePhonebook_vue_vue_type_template_id_6d7474da___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_AudioConferencePhonebook_vue_vue_type_template_id_339afaf8___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
 
 
 
