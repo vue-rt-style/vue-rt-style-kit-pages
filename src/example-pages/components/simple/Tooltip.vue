3462f6ca52348e267046060c7b58a373a1cb3f94
cryingjoker <cryingjoker@me.com>
2020-08-25 18:39:33 +0300

    fix tooltip

--- src/example-pages/components/simple/Tooltip.vue
+++ src/example-pages/components/simple/Tooltip.vue
@@ -33,7 +33,29 @@
 				<rt-col>
 					<rt-row>
 						<rt-col size="3"></rt-col>
-						<rt-col><p  v-tooltip="{'text':'Добавить в избранное'}" class="d-inline-block">tooltip</p></rt-col>
+						<rt-col><p v-tooltip="{'text':'Добавить в избранное'}" class="d-inline-block">tooltip</p></rt-col>
+            <rt-col>
+            <svg
+                v-tooltip="{'text': this.text+this.count,'spec-content':true}"
+                class="number__compare d-block"
+
+                data-v-e259b9e8="" width="20px" height="20px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink">
+              <g data-v-e259b9e8="" id="icons/add_grey" stroke-width="1" fill-rule="evenodd">
+                <g data-v-e259b9e8="" id="Group" transform="translate(2.000000, 3.000000)">
+                  <rect data-v-e259b9e8="" id="Rectangle" x="6" y="8" width="10" height="2"></rect>
+                  <rect data-v-e259b9e8="" id="Rectangle-Copy-2" x="0" y="0" width="12" height="2"></rect>
+                  <rect data-v-e259b9e8="" id="Rectangle-Copy-3" x="0" y="4" width="8" height="2"></rect>
+                  <rect data-v-e259b9e8="" id="Rectangle-Copy-4" x="0" y="8" width="4" height="2"></rect>
+                  <polygon data-v-e259b9e8="" id="Rectangle" transform="translate(11.000000, 9.000000) rotate(90.000000) translate(-11.000000, -9.000000) " points="6 8 16 8 16 10 6 10"></polygon>
+                </g>
+              </g>
+            </svg>
+              {{this.text+this.count}}
+            </rt-col>
+            <rt-col>
+            <button v-on:click="count += 1">сменить реактивный текст</button>
+            </rt-col>
 						<rt-col size="3"></rt-col>
 					</rt-row>
 				</rt-col>
@@ -68,13 +90,17 @@
     name: "AppSlider",
     components: componentsList,
     data: () => ({
-      documentation: {}
+      documentation: {},
+      text: 'это реактивный текст',
+      count: 0
     }),
     mounted() {
       this.documentation = documentation;
     },
     created() {
     },
-    methods: {}
+    methods: {
+
+    }
   };
 </script>
