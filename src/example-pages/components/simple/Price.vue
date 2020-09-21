1301991c4b45b0173fa9156c7ff3ff75ef4c66fe
cryingjoker <cryingjoker@me.com>
2020-09-21 16:28:26 +0300

    new price

--- src/example-pages/components/simple/Price.vue
+++ src/example-pages/components/simple/Price.vue
@@ -1,100 +1,175 @@
 <template>
-  <div class="rt-container">
-    <div class="app-content rt-col-12 rt-col-md-3">
-      <div class="app-title row"><h1 class="col rt-font-hero">Price</h1></div>
-      <div class="app-body">
-        <div><rt-price :v2="true" :is-option="true" :value='750.2' currency='$'/></div>
-
-        <div><rt-price :v2="true" :value='7150.2' :old-value='62166.2234'/></div>
-        <div><rt-price :v2="true" :value='71150.2' :old-value='666.2234'/></div>
-        <div><rt-price :v2="true" :value='713150.2' :old-value='6661242' currency='$'/></div>
-        <div><rt-price :v2="true" :show-float="true" :value='0.2' :old-value='666.2234' currency='$'/></div>
-        <div><rt-price :v2="true" :show-float="true" :value='0.2332' :old-value='666.2234' currency='$'/></div>
-        <div><rt-price :v2="true" :show-float="true" :value='0.2323232' :old-value='666.2234' currency='$'/></div>
-        <div><rt-price :v2="true" :show-float="true" :value='122.2323232' :old-value='666.2234' currency='$'/></div>
-        <div><rt-price :v2="true" :show-float="true" :value='122332.2323232' :old-value='666.2234' currency='$'/></div>
-        <div><rt-price :v2="true" :show-float="true" :value='122332323.2323232' :old-value='666.2234' currency='$'/></div>
-        <div><rt-price :v2="true" :value='1231223169' :old-value='666' :only-price='true' currency='€' /></div>
-        <div class="is-b2c">
-          <div class="row">
-            <div class="rt-col-4 rt-col-md-3">
-              <pre-code text="<rt-price :value='750.2' :old-value='666.2234' currency='$'/>"/>
-            </div>
-            <div class="rt-col-4 rt-col-md-3 rt-md-space-top">
-              <pre-code text="<rt-price :value='750.231' :old-value='123.2'/>"/>
-            </div>
-            <div class="rt-col-4 rt-col-md-3 rt-md-space-top">
-              <pre-code text="<rt-price :show-float='true' :value='18750.32' :old-value='123.2'/>"/>
-            </div>
-            <div class="rt-col-4 rt-col-md-3 rt-md-space-top">
-              <pre-code text="<rt-price :value='750.231'/>"/>
-            </div>
+  <div>
+    <div class="rt-container">
+      <div class="app-content rt-col-12 rt-col-md-3">
+        <div class="app-title row"><h1 class="col rt-font-hero">Price</h1></div>
+        <div class="app-body">
+          <div>
+            <rt-price :v2="true" :is-option="true" :value='750.2' currency='$'/>
           </div>
-
-          <div class="row rt-space-top">
-            <div class="rt-col-4 rt-col-md-3">
-              <pre-code text="<rt-price :value='69' option-label='До' :is-option='true' currency='€'/>"/>
-            </div>
-            <div class="rt-col-4 rt-col-md-3 rt-md-space-top">
-              <pre-code text="<rt-price :value='750.231' time-interval='час.'/>"/>
-            </div>
+          <div>
+            <rt-price :v2="true" :value='7150.2' :old-value='62166.2234'/>
           </div>
-          <div class="row rt-space-top">
-            <div class="rt-col-16 rt-col-md-3">
-              <pre-code text="<rt-price :value='1231223169' :is-option='true' currency='€'/>"/>
-            </div>
+          <div>
+            <rt-price :v2="true" :value='71150.2' :old-value='666.2234'/>
           </div>
-          <div class="row rt-space-top">
-            <div class="rt-col-8 rt-col-md-3">
-              <pre-code text="<rt-price :value='1231223169' :only-price='true' currency='€'/>"/>
-            </div>
-            <div class="rt-col-8 rt-col-md-3">
-              <pre-code text="<rt-price :value='1231223169' :old-value='666' :only-price='true' currency='€'/>"/>
-            </div>
+          <div>
+            <rt-price :v2="true" :value='713150.2' :old-value='6661242' currency='$'/>
           </div>
-          <div class="row rt-space-top">
-            <div class="rt-col-16 rt-col-md-3">
-              <pre-code text="<div>
-                <rt-price :for-game='true' class='color-orange' :value='800' time-interval='сек' currency='Мбит'/>
-                <rt-price :bold-option='true' class='color-white' :for-game='true' :value='800' time-interval='сек' currency='Мбит'/>
-              </div>"/>
-            </div>
+          <div>
+            <rt-price :v2="true" :show-float="true" :value='0.2' :old-value='666.2234' currency='$'/>
           </div>
-        </div>
-        <div class="is-b2b">
-          <div class="row rt-space-top">
-            <div class="rt-col-4 rt-col-md-3">
-              <pre-code text="<rt-price old-price-color='red' :old-value='2500' :value='400.30' :is-option='false' currency='₽' :b2bPrice='true' time-interval='в месяц' class='color-main09' :show-float='true' :trim-hundredth='true'/>"/>
-            </div>
+          <div>
+            <rt-price :v2="true" :show-float="true" :value='0.0332' :old-value='666.2234' currency='$'/>
           </div>
-          <div class="row rt-space-top">
-            <div class="rt-col-4 rt-col-md-3">
-              <rt-price :value='500' currency='₽' :b2b-price='true' time-interval='' class='color-main09' :is-option='true' :epc-price='true'/>
-            </div>
+          <div>
+            <rt-price :v2="true" :show-float="true" :value='0.2323232' :old-value='666.2234' currency='$'/>
+          </div>
+          <div>
+            <rt-price :v2="true" :show-float="true" :value='122.2323232' :old-value='666.2234' currency='$'/>
+          </div>
+          <div>
+            <rt-price :v2="true" :show-float="true" :value='122332.2323232' :old-value='666.2234' currency='$'/>
+          </div>
+          <div>
+            <rt-price :v2="true" :show-float="true" :value='122332323.2323232' :old-value='666.2234' currency='$'/>
+          </div>
+          <div>
+            <rt-price :v2="true" :value='1231223169' :old-value='666' :only-price='true' currency='€'/>
           </div>
         </div>
       </div>
     </div>
-    <div class="rt-space-top25">
-      <documentation-builder :json="documentation" type="components" />
+    <div class="color-block--dark-blue rt-space-vertical2">
+      <div class="rt-container">
+        <rt-row>
+          <rt-col>
+            <div>
+              <rt-price :v2="true" :bright="true" :is-option="true" :value='750.2' currency='$'/>
+            </div>
+            <div>
+              <rt-price :v2="true" :bright="true" :value='7150.2' :old-value='62166.2234'/>
+            </div>
+            <div>
+              <rt-price :v2="true" :bright="true" :value='71150.2' :old-value='666.2234'/>
+            </div>
+            <div>
+              <rt-price :v2="true" :bright="true" :value='713150.2' :old-value='6661242' currency='$'/>
+            </div>
+            <div>
+              <rt-price :v2="true" :bright="true" :show-float="true" :value='0.2' :old-value='666.2234' currency='$'/>
+            </div>
+            <div>
+              <rt-price :v2="true" :bright="true" :show-float="true" :value='0.2332' :old-value='666.2234'
+                        currency='$'/>
+            </div>
+            <div>
+              <rt-price :v2="true" :bright="true" :show-float="true" :value='0.2323232' :old-value='666.2234'
+                        currency='$'/>
+            </div>
+            <div>
+              <rt-price :v2="true" :bright="true" :show-float="true" :value='122.2323232' :old-value='666.2234'
+                        currency='$'/>
+            </div>
+            <div>
+              <rt-price :v2="true" :bright="true" :show-float="true" :value='122332.2323232' :old-value='666.2234'
+                        currency='$'/>
+            </div>
+            <div>
+              <rt-price :v2="true" :bright="true" :show-float="true" :value='122332323.2323232' :old-value='666.2234'
+                        currency='$'/>
+            </div>
+            <div>
+              <rt-price :v2="true" :bright="true" :value='1231223169' :old-value='666' :only-price='true' currency='€'/>
+            </div>
+          </rt-col>
+        </rt-row>
+      </div>
+    </div>
+    <div class="rt-container">
+      <div class="is-b2c">
+        <div class="row">
+          <div class="rt-col-4 rt-col-md-3">
+            <pre-code text="<rt-price :value='750.2' :old-value='666.2234' currency='$'/>"/>
+          </div>
+          <div class="rt-col-4 rt-col-md-3 rt-md-space-top">
+            <pre-code text="<rt-price :value='750.231' :old-value='123.2'/>"/>
+          </div>
+          <div class="rt-col-4 rt-col-md-3 rt-md-space-top">
+            <pre-code text="<rt-price :show-float='true' :value='18750.32' :old-value='123.2'/>"/>
+          </div>
+          <div class="rt-col-4 rt-col-md-3 rt-md-space-top">
+            <pre-code text="<rt-price :value='750.231'/>"/>
+          </div>
+        </div>
+
+        <div class="row rt-space-top">
+          <div class="rt-col-4 rt-col-md-3">
+            <pre-code text="<rt-price :value='69' option-label='До' :is-option='true' currency='€'/>"/>
+          </div>
+          <div class="rt-col-4 rt-col-md-3 rt-md-space-top">
+            <pre-code text="<rt-price :value='750.231' time-interval='час.'/>"/>
+          </div>
+        </div>
+        <div class="row rt-space-top">
+          <div class="rt-col-16 rt-col-md-3">
+            <pre-code text="<rt-price :value='1231223169' :is-option='true' currency='€'/>"/>
+          </div>
+        </div>
+        <div class="row rt-space-top">
+          <div class="rt-col-8 rt-col-md-3">
+            <pre-code text="<rt-price :value='1231223169' :only-price='true' currency='€'/>"/>
+          </div>
+          <div class="rt-col-8 rt-col-md-3">
+            <pre-code text="<rt-price :value='1231223169' :old-value='666' :only-price='true' currency='€'/>"/>
+          </div>
+        </div>
+        <div class="row rt-space-top">
+          <div class="rt-col-16 rt-col-md-3">
+            <pre-code text="<div>
+                <rt-price :for-game='true' class='color-orange' :value='800' time-interval='сек' currency='Мбит'/>
+                <rt-price :bold-option='true' class='color-white' :for-game='true' :value='800' time-interval='сек' currency='Мбит'/>
+              </div>"/>
+          </div>
+        </div>
+      </div>
+      <div class="is-b2b">
+        <div class="row rt-space-top">
+          <div class="rt-col-4 rt-col-md-3">
+            <pre-code
+                text="<rt-price old-price-color='red' :old-value='2500' :value='400.30' :is-option='false' currency='₽' :b2bPrice='true' time-interval='в месяц' class='color-main09' :show-float='true' :trim-hundredth='true'/>"/>
+          </div>
+        </div>
+        <div class="row rt-space-top">
+          <div class="rt-col-4 rt-col-md-3">
+            <rt-price :value='500' currency='₽' :b2b-price='true' time-interval='' class='color-main09'
+                      :is-option='true' :epc-price='true'/>
+          </div>
+        </div>
+      </div>
+      <div class="rt-space-top25">
+        <documentation-builder :json="documentation" type="components"/>
+      </div>
     </div>
   </div>
+
 </template>
 
 <script>
-  import documentation from "@vue-rt-style-kit-atoms-local/components/Price/docs/index.json";
+import documentation from "@vue-rt-style-kit-atoms-local/components/Price/docs/index.json";
 
-  const componentsList = {};
-  export default {
-    name: "AppPrice",
-    components: componentsList,
-    data: () => ({
-      documentation: {}
-    }),
-    mounted() {
-      this.documentation = documentation;
-    },
-    created() {},
-    methods: {}
-  };
+const componentsList = {};
+export default {
+  name: "AppPrice",
+  components: componentsList,
+  data: () => ({
+    documentation: {}
+  }),
+  mounted() {
+    this.documentation = documentation;
+  },
+  created() {
+  },
+  methods: {}
+};
 </script>
