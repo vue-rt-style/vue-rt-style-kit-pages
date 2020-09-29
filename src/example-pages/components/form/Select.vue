fdd0641c57527396010c3ce3e8286a6359ac3f2e
cryingjoker <cryingjoker@me.com>
2020-09-29 10:21:26 +0300

    select v2 add json data

--- src/example-pages/components/form/Select.vue
+++ src/example-pages/components/form/Select.vue
@@ -15,6 +15,7 @@
           </div>
         </div>
         <div class="app-row row">
+          <rt-select-v2 :set-first-active="true" :json="[{value:'50', label:'150 минут'},{value:'150', label:'250 минут'},{value:'250', label:'500 минут'}]" :value="['50']" name="test2" label="Оборудование" :has-content-text="true"></rt-select-v2>
           <rt-select-v2 :set-first-active="true" :value="['50']" type="multiselect" name="test1" label="Оборудование" :has-content-text="true">
             <template slot="content">
               <p class="rt-font-control color-main05 rt-space-top0-half">Пакет входящих минут <br> с любых телефонов России</p>
