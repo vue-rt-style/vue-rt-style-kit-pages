9d8b408f1354c10bc3cad512de9913b4b7766fd9
cryingjoker <cryingjoker@me.com>
2020-10-07 14:02:42 +0300

    deploy docs

--- src/example-pages/components/simple/SystemIcons.vue
+++ src/example-pages/components/simple/SystemIcons.vue
@@ -1,96 +1,7 @@
-<template>
-  <div class="rt-container">
-    <div class="app-content rt-col-12 rt-col-md-3">
-      <div class="app-title">
-        <h1 class="rt-font-hero">
-          System Icons
-        </h1>
-      </div>
-      <div class="app-body">
-        <div class="app-row row">
-          <rt-system-icons name="add large"></rt-system-icons>
-          <rt-system-icons name="add small"></rt-system-icons>
-          <rt-system-icons name="arrow down"></rt-system-icons>
-          <rt-system-icons name="arrow left down"></rt-system-icons>
-          <rt-system-icons name="arrow left"></rt-system-icons>
-          <rt-system-icons name="arrow right up"></rt-system-icons>
-          <rt-system-icons name="arrow right"></rt-system-icons>
-          <rt-system-icons name="arrow up"></rt-system-icons>
-          <rt-system-icons name="check large"></rt-system-icons>
-          <rt-system-icons name="check small"></rt-system-icons>
-          <rt-system-icons name="checklist"></rt-system-icons>
-          <rt-system-icons name="chevron down"></rt-system-icons>
-          <rt-system-icons name="chevron left"></rt-system-icons>
-          <rt-system-icons name="chevron right"></rt-system-icons>
-          <rt-system-icons name="chevron sort"></rt-system-icons>
-          <rt-system-icons name="chevron up"></rt-system-icons>
-          <rt-system-icons name="close large"></rt-system-icons>
-          <rt-system-icons name="close small"></rt-system-icons>
-          <rt-system-icons name="copy"></rt-system-icons>
-          <rt-system-icons name="download"></rt-system-icons>
-          <rt-system-icons name="fast forward 10s"></rt-system-icons>
-          <rt-system-icons name="fast forward 15s"></rt-system-icons>
-          <rt-system-icons name="fast forward 30s"></rt-system-icons>
-          <rt-system-icons name="fast forward 5s"></rt-system-icons>
-          <rt-system-icons name="fast forward"></rt-system-icons>
-          <rt-system-icons name="fast rewind 10s"></rt-system-icons>
-          <rt-system-icons name="fast rewind 15s"></rt-system-icons>
-          <rt-system-icons name="fast rewind 30s"></rt-system-icons>
-          <rt-system-icons name="fast rewind 5s"></rt-system-icons>
-          <rt-system-icons name="fast rewind"></rt-system-icons>
-          <rt-system-icons name="folder add"></rt-system-icons>
-          <rt-system-icons name="folder details"></rt-system-icons>
-          <rt-system-icons name="folder shared"></rt-system-icons>
-          <rt-system-icons name="folder"></rt-system-icons>
-          <rt-system-icons name="geolocation small"></rt-system-icons>
-          <rt-system-icons name="geolocation"></rt-system-icons>
-          <rt-system-icons name="home"></rt-system-icons>
-          <rt-system-icons name="link native"></rt-system-icons>
-          <rt-system-icons name="link"></rt-system-icons>
-          <rt-system-icons name="map"></rt-system-icons>
-          <rt-system-icons name="maximize"></rt-system-icons>
-          <rt-system-icons name="menu + bullets"></rt-system-icons>
-          <rt-system-icons name="menu kebab"></rt-system-icons>
-          <rt-system-icons name="menu"></rt-system-icons>
-          <rt-system-icons name="minimize"></rt-system-icons>
-          <rt-system-icons name="more"></rt-system-icons>
-          <rt-system-icons name="overview 1 screen"></rt-system-icons>
-          <rt-system-icons name="overview 2 screen"></rt-system-icons>
-          <rt-system-icons name="overview 4 screen"></rt-system-icons>
-          <rt-system-icons name="overview 6 screen"></rt-system-icons>
-          <rt-system-icons name="pause"></rt-system-icons>
-          <rt-system-icons name="pin fill"></rt-system-icons>
-          <rt-system-icons name="pin"></rt-system-icons>
-          <rt-system-icons name="play next"></rt-system-icons>
-          <rt-system-icons name="play previous"></rt-system-icons>
-          <rt-system-icons name="play"></rt-system-icons>
-          <rt-system-icons name="progress download"></rt-system-icons>
-          <rt-system-icons name="redo"></rt-system-icons>
-          <rt-system-icons name="remove small"></rt-system-icons>
-          <rt-system-icons name="search"></rt-system-icons>
-          <rt-system-icons name="settings"></rt-system-icons>
-          <rt-system-icons name="speed 1х"></rt-system-icons>
-          <rt-system-icons name="speed 2х"></rt-system-icons>
-          <rt-system-icons name="speed 4х"></rt-system-icons>
-          <rt-system-icons name="speed 6х"></rt-system-icons>
-          <rt-system-icons name="speed 8х"></rt-system-icons>
-          <rt-system-icons name="speed playback"></rt-system-icons>
-          <rt-system-icons name="subtitles fill"></rt-system-icons>
-          <rt-system-icons name="subtitles"></rt-system-icons>
-          <rt-system-icons name="trash"></rt-system-icons>
-          <rt-system-icons name="undo"></rt-system-icons>
-          <rt-system-icons name="upload"></rt-system-icons>
-          <rt-system-icons name="zoom in"></rt-system-icons>
-          <rt-system-icons name="zoom out"></rt-system-icons>
 
-        </div>
 
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
+<script type="text/jsx">
+import names from "@vue-rt-style-kit-atoms-local/components/SystemIcons/docs/names.json";
 const componentsList = {};
 
 export default {
@@ -99,6 +10,51 @@ export default {
   mounted() {},
   created() {},
 
-  methods: {}
+  methods: {},
+  computed:{
+    renderList(){
+      return names.map((i)=>{
+        return <rt-table-row>
+          <rt-table-item>
+            <rt-system-icons name={i.name}></rt-system-icons>
+          </rt-table-item>
+          <rt-table-item>
+            {i.name}
+          </rt-table-item>
+          <rt-table-item>
+            {i.label}
+          </rt-table-item>
+        </rt-table-row>
+      })
+    },
+    renderTable(){
+
+    },
+
+  },
+  render(){
+    return <div class="rt-container">
+      <div class="app-content rt-col-12 rt-col-md-3">
+        <div class="app-title">
+          <h1 class="rt-font-hero">
+            System Icons
+          </h1>
+        </div>
+        <div class="app-body">
+          <div class="app-row row">
+
+            <rt-table class="rt-font-label">
+              <template slot="header">
+                <rt-table-head-item>icon</rt-table-head-item>
+                <rt-table-head-item>name</rt-table-head-item>
+                <rt-table-head-item>label</rt-table-head-item>
+              </template>
+              <template slot="body">{this.renderList}</template>
+            </rt-table>
+          </div>
+        </div>
+      </div>
+    </div>
+  }
 };
 </script>
