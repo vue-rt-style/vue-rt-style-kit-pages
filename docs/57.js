7ac17d0ac28dca3a00f5be59898f3e0d0fee328b
cryingjoker <cryingjoker@me.com>
2020-10-15 14:59:53 +0300

    deploy docs

--- docs/57.js
+++ docs/57.js
@@ -49,6 +49,46 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 // import documentation from "@vue-rt-style-kit-molecules-local/components/Tabs/docs/index.json";
 // import componentsList from "../../componentsList";
 /* harmony default export */ __webpack_exports__["default"] = ({
@@ -69,9 +109,9 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=172e10c4&":
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=2f9d80c8&":
 /*!************************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=172e10c4& ***!
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=2f9d80c8& ***!
   \************************************************************************************************************************************************************************************************************************/
 /*! exports provided: render, staticRenderFns */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
@@ -80,7 +120,7 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"rt-container"},[_c('div',{staticClass:"app-content rt-col-12 rt-col-md-3"},[_vm._m(0),_vm._v(" "),_c('rt-row',{staticClass:"app-row"},[_c('rt-col',{attrs:{"size":"12"}},[_c('rt-tabs-slider',{attrs:{"name":"tabs-slider-1"}},[_c('rt-tabs-slider-item',{attrs:{"label":"Заголовок пункта 1","tabs-slider-name":"tabs-slider-1"}},[_c('template',{slot:"content"},[_c('p',{staticClass:"rt-font-h2"},[_vm._v("Заголовок блока")]),_vm._v(" "),_c('p',{staticClass:"sp-t-2"},[_vm._v("На краю дороги стоял дуб. Он был, вероятно, в десять раз старше берез,\n                  составлявших лес, в десять раз толще и в два раза выше каждой березы.")])]),_vm._v(" "),_c('template',{slot:"image"},[_c('img',{attrs:{"src":"https://m1.35photo.pro/photos_main/1005/5025612.jpg"}})])],2)],1),_vm._v(" "),_c('rt-tabs-slider-item',{attrs:{"label":"Заголовок пункта 2","tabs-slider-name":"tabs-slider-1"}},[_c('template',{slot:"content"},[_c('p',{staticClass:"rt-font-h2"},[_vm._v("Заголовок блока 2")]),_vm._v(" "),_c('p',{staticClass:"sp-t-2"},[_vm._v("На краю дороги стоял дуб. Он был, вероятно, в десять раз старше берез,\n                составлявших лес, в десять раз толще и в два раза выше каждой березы.")])]),_vm._v(" "),_c('template',{slot:"image"},[_c('img',{attrs:{"src":"https://m1.35photo.pro/photos_main/1007/5039020.jpg"}})])],2)],1)],1)],1),_vm._v(" "),_c('documentation-builder',{attrs:{"json":_vm.documentation,"type":"components"}})],1)}
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"rt-container"},[_c('div',{staticClass:"app-content rt-col-12 rt-col-md-3"},[_vm._m(0),_vm._v(" "),_c('rt-row',{staticClass:"app-row"},[_c('rt-col',{attrs:{"size":"12"}},[_c('rt-tabs-slider',{attrs:{"name":"tabs-slider-1"}},[_c('rt-tabs-slider-item',{attrs:{"label":"Заголовок пункта 1","tabs-slider-name":"tabs-slider-1"}},[_c('template',{slot:"content"},[_c('p',{staticClass:"rt-font-h2"},[_vm._v("Заголовок блока")]),_vm._v(" "),_c('p',{staticClass:"sp-t-2"},[_vm._v("На краю дороги стоял дуб. Он был, вероятно, в десять раз старше берез,\n                  составлявших лес, в десять раз толще и в два раза выше каждой березы.")])]),_vm._v(" "),_c('template',{slot:"image"},[_c('img',{attrs:{"src":"https://m1.35photo.pro/photos_main/1005/5025612.jpg"}})])],2)],1),_vm._v(" "),_c('rt-tabs-slider-item',{attrs:{"label":"Заголовок пункта 2","tabs-slider-name":"tabs-slider-1"}},[_c('template',{slot:"content"},[_c('p',{staticClass:"rt-font-h2"},[_vm._v("Заголовок блока 2")]),_vm._v(" "),_c('p',{staticClass:"sp-t-2"},[_vm._v("На краю дороги стоял дуб. Он был, вероятно, в десять раз старше берез,\n                составлявших лес, в десять раз толще и в два раза выше каждой березы.")])]),_vm._v(" "),_c('template',{slot:"image"},[_c('img',{attrs:{"src":"https://m1.35photo.pro/photos_main/1007/5039020.jpg"}})])],2),_vm._v(" "),_c('rt-tabs-slider-item',{attrs:{"label":"Заголовок пункта 3","tabs-slider-name":"tabs-slider-1"}},[_c('template',{slot:"content"},[_c('p',{staticClass:"rt-font-h2"},[_vm._v("Заголовок блока 3")]),_vm._v(" "),_c('p',{staticClass:"sp-t-2"},[_vm._v("На краю дороги стоял дуб. Он был, вероятно, в десять раз старше берез,\n                составлявших лес, в десять раз толще и в два раза выше каждой березы.")])]),_vm._v(" "),_c('template',{slot:"image"},[_c('img',{attrs:{"src":"https://m1.35photo.pro/photos_main/1005/5026518.jpg"}})])],2),_vm._v(" "),_c('rt-tabs-slider-item',{attrs:{"label":"Заголовок пункта 4","tabs-slider-name":"tabs-slider-1"}},[_c('template',{slot:"content"},[_c('p',{staticClass:"rt-font-h2"},[_vm._v("Заголовок блока 4")]),_vm._v(" "),_c('p',{staticClass:"sp-t-2"},[_vm._v("На краю дороги стоял дуб. Он был, вероятно, в десять раз старше берез,\n                составлявших лес, в десять раз толще и в два раза выше каждой березы.")])]),_vm._v(" "),_c('template',{slot:"image"},[_c('img',{attrs:{"src":"https://m1.35photo.pro/photos_main/1000/5004154.jpg"}})])],2),_vm._v(" "),_c('rt-tabs-slider-item',{attrs:{"label":"Заголовок пункта 5","tabs-slider-name":"tabs-slider-1"}},[_c('template',{slot:"content"},[_c('p',{staticClass:"rt-font-h2"},[_vm._v("Заголовок блока 5")]),_vm._v(" "),_c('p',{staticClass:"sp-t-2"},[_vm._v("На краю дороги стоял дуб. Он был, вероятно, в десять раз старше берез,\n                составлявших лес, в десять раз толще и в два раза выше каждой березы.")])]),_vm._v(" "),_c('template',{slot:"image"},[_c('img',{attrs:{"src":"https://m1.35photo.pro/photos_main/999/4999134.jpg"}})])],2),_vm._v(" "),_c('rt-tabs-slider-item',{attrs:{"label":"Заголовок пункта 6","tabs-slider-name":"tabs-slider-1"}},[_c('template',{slot:"content"},[_c('p',{staticClass:"rt-font-h2"},[_vm._v("Заголовок блока 6")]),_vm._v(" "),_c('p',{staticClass:"sp-t-2"},[_vm._v("На краю дороги стоял дуб. Он был, вероятно, в десять раз старше берез,\n                составлявших лес, в десять раз толще и в два раза выше каждой березы.")])]),_vm._v(" "),_c('template',{slot:"image"},[_c('img',{attrs:{"src":"https://m1.35photo.pro/photos_main/995/4977757.jpg"}})])],2)],1)],1)],1),_vm._v(" "),_c('documentation-builder',{attrs:{"json":_vm.documentation,"type":"components"}})],1)}
 var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"app-title"},[_c('h1',{staticClass:"rt-font-hero"},[_vm._v("\n        Tabs Slider\n      ")])])}]
 
 
@@ -96,7 +136,7 @@ var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _TabsSlider_vue_vue_type_template_id_172e10c4___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./TabsSlider.vue?vue&type=template&id=172e10c4& */ "./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=172e10c4&");
+/* harmony import */ var _TabsSlider_vue_vue_type_template_id_2f9d80c8___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./TabsSlider.vue?vue&type=template&id=2f9d80c8& */ "./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=2f9d80c8&");
 /* harmony import */ var _TabsSlider_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./TabsSlider.vue?vue&type=script&lang=js& */ "./src/example-pages/components/group/TabsSlider.vue?vue&type=script&lang=js&");
 /* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
@@ -108,8 +148,8 @@ __webpack_require__.r(__webpack_exports__);
 
 var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
   _TabsSlider_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
-  _TabsSlider_vue_vue_type_template_id_172e10c4___WEBPACK_IMPORTED_MODULE_0__["render"],
-  _TabsSlider_vue_vue_type_template_id_172e10c4___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  _TabsSlider_vue_vue_type_template_id_2f9d80c8___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _TabsSlider_vue_vue_type_template_id_2f9d80c8___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
   null,
   null,
@@ -135,19 +175,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=172e10c4&":
+/***/ "./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=2f9d80c8&":
 /*!******************************************************************************************!*\
-  !*** ./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=172e10c4& ***!
+  !*** ./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=2f9d80c8& ***!
   \******************************************************************************************/
 /*! exports provided: render, staticRenderFns */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_TabsSlider_vue_vue_type_template_id_172e10c4___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./TabsSlider.vue?vue&type=template&id=172e10c4& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=172e10c4&");
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_TabsSlider_vue_vue_type_template_id_172e10c4___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_TabsSlider_vue_vue_type_template_id_2f9d80c8___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./TabsSlider.vue?vue&type=template&id=2f9d80c8& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/example-pages/components/group/TabsSlider.vue?vue&type=template&id=2f9d80c8&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_TabsSlider_vue_vue_type_template_id_2f9d80c8___WEBPACK_IMPORTED_MODULE_0__["render"]; });
 
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_TabsSlider_vue_vue_type_template_id_172e10c4___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_TabsSlider_vue_vue_type_template_id_2f9d80c8___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
 
 
 
