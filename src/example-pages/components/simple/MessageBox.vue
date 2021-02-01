d1a510abf7f90b688be8a0599071db90d4316a16
Sergey.Sarkisyants <Sergey.Sarkisyants@south.rt.ru>
2021-02-01 15:19:21 +0300

    RTRUB2B-4868 Docs for Message box component

--- src/example-pages/components/simple/MessageBox.vue
+++ src/example-pages/components/simple/MessageBox.vue
@@ -1,12 +1,26 @@
 <template>
-  <rt-message-box theme="light" link="/eng" :timeout=1>
-    <p class="rt-font-small-paragraph rt-font-bold">Не нашли, что искали?</p>
-    <p class="rt-font-control sp-t-0-2 md-sp-t-0-1">Больше информации и предложений на нашем сайте</p>
-  </rt-message-box>
+  <div>
+    <rt-message-box :timeout=1>
+      <p class="rt-font-small-paragraph rt-font-bold">Не нашли, что искали?</p>
+      <p class="rt-font-control sp-t-0-2 md-sp-t-0-1">Больше информации и предложений на нашем сайте</p>
+    </rt-message-box>
+    <div class="rt-container rt-space-top25">
+      <documentation-builder :json="documentation" type="components"/>
+    </div>
+  </div>
 </template>
 
 <script>
+  import documentation from "@vue-rt-style-kit-molecules-local/components/MessageBox/docs/index.json";
+  import componentsList from "../../componentsList";
   export default {
-    name: "AppMessageBox"
+    name: "AppMessageBox",
+    components: componentsList,
+    data: () => ({
+      documentation: {}
+    }),
+    mounted() {
+      this.documentation = documentation;
+    },
   }
 </script>
