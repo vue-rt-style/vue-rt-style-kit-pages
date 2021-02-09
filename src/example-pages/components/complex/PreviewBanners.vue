dc8ca2e6a7378de242648a227d004a1ddd8c54eb
cryingjoker <cryingjoker@me.com>
2021-02-09 12:34:34 +0300

    deploy docs

--- src/example-pages/components/complex/PreviewBanners.vue
+++ src/example-pages/components/complex/PreviewBanners.vue
@@ -29,6 +29,27 @@
           <rt-button color="orange">Узнать подробности</rt-button>
         </template>
       </rt-preview-banner>'></pre-code>
+      <br>
+      <br>
+      <pre-code text='<rt-preview-banner background="dark-slate" color="white">
+        <template slot="image">
+          <rt-img src="https://vue-rt-style.github.io/vue-rt-style-kit-pages/static/example-images/previewImage/previewImage.png"
+                  x2-src="https://vue-rt-style.github.io/vue-rt-style-kit-pages/static/example-images/previewImage/previewImage2x.png"
+                  td-src="https://vue-rt-style.github.io/vue-rt-style-kit-pages/static/example-images/previewImage/previewImage-td.png"
+                  md-src="https://vue-rt-style.github.io/vue-rt-style-kit-pages/static/example-images/previewImage/previewImage-md.png"
+          ></rt-img>
+        </template>
+        <template slot="date"><div class="d-flex flex-v-center"><rt-system-icons name="calendar"></rt-system-icons><span class="sp-l-0-2">1  мая по 31 декабря 2020 г.</span></div></template>
+        <template slot="label">
+          <h3 class="rt-font-h3">Заголовок в одну строку</h3>
+        </template>
+        <template slot="content">
+          <p class="rt-font-small-paragraph">Lorem ipsum — классический текст-«рыба». Является искажённым отрывком из философского трактата Марка Туллия Цицерона «О пределах добра и зла».</p>
+        </template>
+        <template slot="footer">
+          <rt-button color="orange">Узнать подробности</rt-button>
+        </template>
+      </rt-preview-banner>'></pre-code>
     </div>
 
     <div class="rt-container rt-space-top25">
