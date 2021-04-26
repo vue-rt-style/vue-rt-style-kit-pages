35abf7d4c39bfd72060d1333fba74a9ca9957cfb
cryingjoker <cryingjoker@me.com>
2021-04-26 11:45:34 +0300

    deploy docs

--- src/example-pages/components/form/Switch.vue
+++ src/example-pages/components/form/Switch.vue
@@ -9,14 +9,53 @@
       <div class="app-body">
         <div class="app-row row">
           <p class="rt-font-h3 rt-col">
-            switch
+            switch v2
           </p>
         </div>
         <div class="app-row row">
+          <rt-col size="2">
+            <p>Color</p>
+            <select name="" v-model="color" id="">
+              <option value="orange">orange</option>
+              <option value="purple">purple</option>
+            </select>
+          </rt-col>
+          <rt-col size="2">
+            <p>disabled</p>
+            <input type="checkbox" v-model="disabled"/>
+          </rt-col>
+          <rt-col size="2">
+            <p>bright</p>
+            <input type="checkbox" v-model="bright"/>
+          </rt-col>
+          <rt-col size="2">
+            <p>invert</p>
+            <input type="checkbox" v-model="invert"/>
+          </rt-col>
+        </div>
+        <div class="app-row row">
+          <label for="test11" class="d-inline-block sp-b-4">
+            <p>Отдельный лейбл для свитча</p>
+          </label>
+        </div>
+        <div class="app-row row">
+
           <div class="rt-col">
-            <rt-switch-v2>Интернет</rt-switch-v2>
-            <rt-switch :is-orange="true">Интернет</rt-switch>
+
+              <rt-switch-v2 id="test11" :disabled="disabled? true : false" :bright="bright" :invert="invert" :color="color">Интернет</rt-switch-v2>
+
+
           </div>
+        </div>
+        <div class="app-row row">
+          <p class="rt-font-h3 rt-col">
+            switch v1
+          </p>
+        </div>
+
+
+        <div class="app-row row">
+          <rt-switch :is-orange="true">Интернет</rt-switch>
         </div>
         <div class="app-row row">
           <p class="rt-font-h3 rt-col">
@@ -46,7 +85,11 @@
     name: "AppSwitch",
     components: componentsList,
     data: () => ({
-      documentation: {}
+      documentation: {},
+      color: 'orange',
+      disabled: false,
+      bright: false,
+      invert: false
     }),
     mounted() {
       this.documentation = documentation;
