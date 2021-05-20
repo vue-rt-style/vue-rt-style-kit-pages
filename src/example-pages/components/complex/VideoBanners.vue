d3fba851fc2466b48e046c820e4dfcaf9fbe2d37
cryingjoker <cryingjoker@me.com>
2021-05-20 15:38:37 +0300

    deploy docs

--- src/example-pages/components/complex/VideoBanners.vue
+++ src/example-pages/components/complex/VideoBanners.vue
@@ -10,7 +10,8 @@
     </div>
     <div>
       <pre-code text='
-      <rt-video-banner src="https://www.apple.com/105/media/us/airpods-max/2020/996b980b-3131-44f1-af6c-fe72f9b3bfb5/anim/supercut/large_2x.mp4">
+      <rt-video-banner preload-image="https://www.apple.com/v/airpods-max/c/images/overview/design_mesh__cleb6oo85bo2_xlarge.jpg"
+      src="https://www.apple.com/105/media/us/airpods-max/2020/996b980b-3131-44f1-af6c-fe72f9b3bfb5/anim/supercut/large_2x.mp4">
         <template slot="content">
           <div class="md-color-block--dark-slate md-sp-t-1-2 md-sp-b-2 flex-fill">
           <div class="rt-container">
@@ -34,7 +35,7 @@
       <div class="sp-b-5"></div>
       <div class="sp-b-5"></div>
       <div class="sp-b-5"></div>
-      <pre-code text='<rt-video-banner mobile-image="https://www.apple.com/ru/iphone-12/images/overview/accessories/magsafe__emhukwz2mhiu_large_2x.jpg" src="https://www.apple.com/105/media/ww/iphone-12/2020/7f5b7de7-9f8c-41eb-bf3b-f294773108e6/anim/hero-purple/non-5g/large_2x.mp4">
+      <pre-code text='<rt-video-banner preload-image="https://www.apple.com/v/airpods-max/c/images/overview/design_mesh__cleb6oo85bo2_xlarge.jpg" mobile-image="https://www.apple.com/ru/iphone-12/images/overview/accessories/magsafe__emhukwz2mhiu_large_2x.jpg" src="https://www.apple.com/105/media/ww/iphone-12/2020/7f5b7de7-9f8c-41eb-bf3b-f294773108e6/anim/hero-purple/non-5g/large_2x.mp4">
         <template slot="content">
           <div class="md-color-block--dark-slate md-sp-t-1-2 md-sp-b-2 flex-fill">
             <div class="rt-container">
@@ -52,7 +53,9 @@
           </div>
         </template>
       </rt-video-banner>'></pre-code>
-      <pre-code text='<rt-video-banner full-src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" src="https://www.apple.com/105/media/us/imac-24/2021/5e004d75-3ad6-4bb9-ab59-41f891fc52f0/anim/colors-hero/large_2x.mp4">
+      <pre-code text='<rt-video-banner
+      preload-image="https://www.apple.com/v/airpods-max/c/images/overview/design_mesh__cleb6oo85bo2_xlarge.jpg"
+      full-src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" src="https://www.apple.com/105/media/us/imac-24/2021/5e004d75-3ad6-4bb9-ab59-41f891fc52f0/anim/colors-hero/large_2x.mp4">
         <template slot="content">
           <div class="md-color-block--dark-slate md-sp-t-1-2 md-sp-b-2 flex-fill">
             <div class="rt-container">
