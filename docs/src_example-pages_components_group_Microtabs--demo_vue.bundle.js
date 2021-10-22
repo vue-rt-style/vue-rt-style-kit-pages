68d36a8b32e72f349d8ae324dc384d3c7208f688
cryingjoker <cryingjoker@me.com>
2021-10-22 10:38:27 +0300

    deploy docs

--- docs/src_example-pages_components_group_Microtabs--demo_vue.bundle.js
+++ docs/src_example-pages_components_group_Microtabs--demo_vue.bundle.js
@@ -395,7 +395,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var _Microtabs_demo_vue_vue_type_template_id_1645fa95___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Microtabs--demo.vue?vue&type=template&id=1645fa95& */ "./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=1645fa95&");
+/* harmony import */ var _Microtabs_demo_vue_vue_type_template_id_366a7456___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Microtabs--demo.vue?vue&type=template&id=366a7456& */ "./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=366a7456&");
 /* harmony import */ var _Microtabs_demo_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Microtabs--demo.vue?vue&type=script&lang=js& */ "./src/example-pages/components/group/Microtabs--demo.vue?vue&type=script&lang=js&");
 /* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
@@ -412,8 +412,8 @@ if (style0.__inject__) style0.__inject__(context)
 
 var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
   _Microtabs_demo_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
-  _Microtabs_demo_vue_vue_type_template_id_1645fa95___WEBPACK_IMPORTED_MODULE_0__.render,
-  _Microtabs_demo_vue_vue_type_template_id_1645fa95___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
+  _Microtabs_demo_vue_vue_type_template_id_366a7456___WEBPACK_IMPORTED_MODULE_0__.render,
+  _Microtabs_demo_vue_vue_type_template_id_366a7456___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
   false,
   injectStyles,
   null,
@@ -452,25 +452,25 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=1645fa95&":
+/***/ "./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=366a7456&":
 /*!***********************************************************************************************!*\
-  !*** ./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=1645fa95& ***!
+  !*** ./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=366a7456& ***!
   \***********************************************************************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "render": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Microtabs_demo_vue_vue_type_template_id_1645fa95___WEBPACK_IMPORTED_MODULE_0__.render),
-/* harmony export */   "staticRenderFns": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Microtabs_demo_vue_vue_type_template_id_1645fa95___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns)
+/* harmony export */   "render": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Microtabs_demo_vue_vue_type_template_id_366a7456___WEBPACK_IMPORTED_MODULE_0__.render),
+/* harmony export */   "staticRenderFns": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Microtabs_demo_vue_vue_type_template_id_366a7456___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns)
 /* harmony export */ });
-/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Microtabs_demo_vue_vue_type_template_id_1645fa95___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Microtabs--demo.vue?vue&type=template&id=1645fa95& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=1645fa95&");
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Microtabs_demo_vue_vue_type_template_id_366a7456___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Microtabs--demo.vue?vue&type=template&id=366a7456& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=366a7456&");
 
 
 /***/ }),
 
-/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=1645fa95&":
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=366a7456&":
 /*!**************************************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=1645fa95& ***!
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/example-pages/components/group/Microtabs--demo.vue?vue&type=template&id=366a7456& ***!
   \**************************************************************************************************************************************************************************************************************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
