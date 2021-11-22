1155c1de1ad03b5be2a0bcdf49d126738be0d2ea
cryingjoker <cryingjoker@me.com>
2021-11-22 15:41:54 +0300

    deploy docs

--- bin/local/index.js
+++ bin/local/index.js
@@ -24,7 +24,7 @@ const app = express();
 // console.log('config',webpackConfig)
 if(isProductionBuuild){
   webpackConfig.output.publicPath = './'
-  webpackConfig.output.assetModuleFilename =  'vue-rt-style-kit-pages/static/[name][ext]'
+  webpackConfig.output.assetModuleFilename =  'static/[name][ext]'
 }
 const compiler = webpack(webpackConfig);
 let devMiddleware;
